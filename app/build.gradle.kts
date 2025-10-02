6aaafdcd1524cd167acb3d3577bf3f99af92fd30
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-02 13:54:29 -0600

    Config gradle libraries part. 28.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,11 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation(libs.androidx.core.splashscreen)
+
+    implementation(libs.koin.android)
+    implementation(libs.koin.androidx.compose)
+
+    implementation(libs.logging.interceptor)
 }
