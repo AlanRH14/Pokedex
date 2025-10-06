1bb5db02f8468db11e5921d435aaa020bf473eca
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-06 16:52:13 -0600

    Config module data part. 19.

--- app/src/main/java/com/example/pokedex/data/models/Sprites.kt
+++ app/src/main/java/com/example/pokedex/data/models/Sprites.kt
@@ -6,6 +6,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Sprites(
     @SerialName("front_default") val frontDefault: String? = null,
-    val backDefault: String? = null,
+    @SerialName("back_default") val backDefault: String? = null,
     val other: OtherSprits? = null
 )
