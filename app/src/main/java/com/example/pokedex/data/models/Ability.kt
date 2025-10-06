60c720e137e31762e1e3be362ea22318c15fbd0e
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-06 17:07:57 -0600

    Config module data part. 43.

--- app/src/main/java/com/example/pokedex/data/models/Ability.kt
+++ app/src/main/java/com/example/pokedex/data/models/Ability.kt
@@ -1,8 +1,9 @@
 package com.example.pokedex.data.models
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Ability(
-    val ability: AbilityInfo? = null
+    @SerialName("ability") val ability: AbilityInfo? = null
 )
