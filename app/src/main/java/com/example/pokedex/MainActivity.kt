ddfa8c73af0bf0ed99e0db59fa4de18765bea437
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-06 12:47:59 -0600

    Config splash part. 14.

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.pokedex
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,11 +12,49 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.pokedex.ui.theme.PokedexTheme
 
 class MainActivity : ComponentActivity() {
+    private var isSplashScreenVisible = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition { isSplashScreenVisible }
+            setOnExitAnimationListener { splashProvider ->
+                val zoomX = ObjectAnimator.ofFloat(
+                    splashProvider.iconView,
+                    "scaleX",
+                    0.4F,
+                    0F
+                )
+
+                val zoomY = ObjectAnimator.ofFloat(
+                    splashProvider.iconView,
+                    "scaleY",
+                    0.4F,
+                    0F
+                )
+
+                zoomX.apply {
+                    duration = 300
+                    doOnEnd {
+                        splashProvider.remove()
+                    }
+                    start()
+                }
+                zoomY.apply {
+                    duration = 300
+                    doOnEnd {
+                        splashProvider.remove()
+                    }
+                    start()
+                }
+            }
+        }
         enableEdgeToEdge()
         setContent {
             PokedexTheme {
