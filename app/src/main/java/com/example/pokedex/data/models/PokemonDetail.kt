22eff4f2f32bc085606795b1d0ca87150ef710e3
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-06 16:44:41 -0600

    Config module data part. 13.

--- app/src/main/java/com/example/pokedex/data/models/PokemonDetail.kt
+++ app/src/main/java/com/example/pokedex/data/models/PokemonDetail.kt
@@ -1,11 +1,12 @@
 package com.example.pokedex.data.models
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class PokemonDetail(
-    val id: Long = 0,
-    val name: String? = null,
+    @SerialName("id") val id: Long = 0,
+    @SerialName("name") val name: String? = null,
     val height: Long? = null,
     val weight: Long? = null,
     val sprites: Sprites? = null,
