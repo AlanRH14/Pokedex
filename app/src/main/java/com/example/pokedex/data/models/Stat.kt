e49386795ec0aeb1e5384e63d9334360e883a8de
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-06 17:04:03 -0600

    Config module data part. 33.

--- app/src/main/java/com/example/pokedex/data/models/Stat.kt
+++ app/src/main/java/com/example/pokedex/data/models/Stat.kt
@@ -1,5 +1,8 @@
 package com.example.pokedex.data.models
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Stat(
     val baseStat: Long? = null,
     val stat: StatInfo? = null
