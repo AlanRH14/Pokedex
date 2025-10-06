976726aaa795bacfa3feca8b942091cc6164821c
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-06 16:58:36 -0600

    Config module data part. 26.

--- app/src/main/java/com/example/pokedex/data/models/OfficialArtwork.kt
+++ app/src/main/java/com/example/pokedex/data/models/OfficialArtwork.kt
@@ -1,8 +1,9 @@
 package com.example.pokedex.data.models
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class OfficialArtwork(
-    val frontDefault: String? = null
+    @SerialName("front_default") val frontDefault: String? = null
 )
