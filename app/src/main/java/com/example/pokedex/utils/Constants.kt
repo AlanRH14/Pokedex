4b3fb47be66631599f114194fbf14bcbed0859a7
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-07 17:03:50 -0600

    Config module data part. 57.

--- app/src/main/java/com/example/pokedex/utils/Constants.kt
+++ app/src/main/java/com/example/pokedex/utils/Constants.kt
@@ -3,5 +3,6 @@ package com.example.pokedex.utils
 object Constants {
     const val BASE_URL = "https://pokeapi.co/api/v2/"
     const val LIMIT_QUERY = "limit"
+    const val OFFSET_QUERY = "offset"
     const val NAME_PATH = "name"
 }
