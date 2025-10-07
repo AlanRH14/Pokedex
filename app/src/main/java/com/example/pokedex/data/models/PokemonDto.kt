0ca6bcfc7595d95bbbddded84a02fc020a851f70
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-07 13:01:21 -0600

    Config retrofit part. 11.

--- app/src/main/java/com/example/pokedex/data/models/PokemonDto.kt
+++ app/src/main/java/com/example/pokedex/data/models/PokemonDto.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Pokemon(
+data class PokemonDto(
     @SerialName("id") val id: Long = 0,
     @SerialName("name") val name: String? = null,
     @SerialName("url") val url: String? = null
