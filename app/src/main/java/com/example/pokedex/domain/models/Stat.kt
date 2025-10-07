bb0d2ffe58b2f718a3a14677df32c05d7092e8b3
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-07 17:25:25 -0600

    Config module domain part. 15.

--- app/src/main/java/com/example/pokedex/domain/models/Stat.kt
+++ app/src/main/java/com/example/pokedex/domain/models/Stat.kt
@@ -1,9 +1,6 @@
 package com.example.pokedex.domain.models
 
-import com.example.pokedex.data.models.StatInfo
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-
 data class Stat(
-    val baseStat: Long? = null, val stat: StatInfo? = null
+    val baseStat: Long? = null,
+    val stat: String? = null
 )
