571985919de059215660965272a360e2c2824a53
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-07 17:08:54 -0600

    Config module domain part. 2.

---
+++ app/src/main/java/com/example/pokedex/domain/models/AbilityInfo.kt
@@ -0,0 +1,8 @@
+package com.example.pokedex.domain.models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+data class AbilityInfo(
+    val name: String? = null
+)
