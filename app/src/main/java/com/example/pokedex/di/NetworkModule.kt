01b2762106a0554bd69aed5377b660faba3b6b6a
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-07 11:58:16 -0600

    Config retrofit part. 3.

--- app/src/main/java/com/example/pokedex/di/NetworkModule.kt
+++ app/src/main/java/com/example/pokedex/di/NetworkModule.kt
@@ -1,12 +1,22 @@
 package com.example.pokedex.di
 
+import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
+import java.util.concurrent.TimeUnit
 
 val networkModule = module {
     single {
         HttpLoggingInterceptor().apply {
             level = HttpLoggingInterceptor.Level.BODY
         }
+    }
+
+    single {
+        OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .addInterceptor(get<HttpLoggingInterceptor>())
+            .build()
     }
 }
