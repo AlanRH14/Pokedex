9ff55a4eb4c69a8e0e970f443e1aca2ea7ff5f83
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-07 13:36:23 -0600

    Config retrofit part. 13.

---
+++ app/src/main/java/com/example/pokedex/data/remote/PokemonApi.kt
@@ -0,0 +1,14 @@
+package com.example.pokedex.data.remote
+
+import com.example.pokedex.data.models.PokemonDto
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface PokemonApi {
+
+    @GET("pokemon")
+    suspend fun fetchPokemonList(
+        @Query("limit") limit: Int = 20,
+        @Query("offset") offset: Int = 0,
+    ): List<PokemonDto>
+}
