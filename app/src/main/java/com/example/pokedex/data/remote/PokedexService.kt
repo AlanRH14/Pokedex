c840579a9a7cddd059eb9d128f00b8e281d41971
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-07 17:04:26 -0600

    Config module data part. 58.

--- app/src/main/java/com/example/pokedex/data/remote/PokedexService.kt
+++ app/src/main/java/com/example/pokedex/data/remote/PokedexService.kt
@@ -4,6 +4,7 @@ import com.example.pokedex.data.models.PokemonDetailDto
 import com.example.pokedex.data.models.PokemonDto
 import com.example.pokedex.utils.Constants.LIMIT_QUERY
 import com.example.pokedex.utils.Constants.NAME_PATH
+import com.example.pokedex.utils.Constants.OFFSET_QUERY
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -13,7 +14,7 @@ interface PokedexService {
     @GET("pokemon")
     suspend fun fetchPokemonList(
         @Query(LIMIT_QUERY) limit: Int = 20,
-        @Query("offset") offset: Int = 0,
+        @Query(OFFSET_QUERY) offset: Int = 0,
     ): List<PokemonDto>
 
     @GET("pokemon/{name}")
