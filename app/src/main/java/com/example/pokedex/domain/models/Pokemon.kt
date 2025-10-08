2dc2b48d94e8fcd3524dcf021d313ed65f43e73b
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-08 08:10:15 -0600

    Config module domain part. 30.

--- app/src/main/java/com/example/pokedex/domain/models/Pokemon.kt
+++ app/src/main/java/com/example/pokedex/domain/models/Pokemon.kt
@@ -1,8 +1,5 @@
 package com.example.pokedex.domain.models
 
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-
 data class Pokemon(
     val id: Long = 0,
     val name: String? = null,
