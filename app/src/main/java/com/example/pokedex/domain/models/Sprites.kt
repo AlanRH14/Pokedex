c038a6e5e2c8dffac056fa135a1822d9c5b202dc
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-08 08:11:21 -0600

    Config module domain part. 33.

--- app/src/main/java/com/example/pokedex/domain/models/Sprites.kt
+++ app/src/main/java/com/example/pokedex/domain/models/Sprites.kt
@@ -1,8 +1,6 @@
 package com.example.pokedex.domain.models
 
 import com.example.pokedex.data.models.OtherSprites
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
 
 data class Sprites(
     val frontDefault: String? = null,
