92ecdf64fda833cd391ab0d8df3282a75e728f47
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-08 16:46:00 -0600

    Config module data part. 80.

--- app/src/main/java/com/example/pokedex/data/repository/PokemonDetailImpl.kt
+++ app/src/main/java/com/example/pokedex/data/repository/PokemonDetailImpl.kt
@@ -1,7 +1,7 @@
 package com.example.pokedex.data.repository
 
+import com.example.pokedex.data.models.PokemonDetailDto
 import com.example.pokedex.data.remote.PokedexService
-import com.example.pokedex.domain.models.PokemonDetail
 import com.example.pokedex.domain.repository.PokemonDetailRepository
 import com.example.pokedex.utils.Resource
 import kotlinx.coroutines.flow.Flow
@@ -11,7 +11,7 @@ class PokemonDetailImpl(
     private val pokedexService: PokedexService
 ): PokemonDetailRepository {
 
-    override fun fetchPokemonDetail(name: String): Flow<Resource<PokemonDetail>> = flow {
+    override fun fetchPokemonDetail(name: String): Flow<Resource<PokemonDetailDto>> = flow {
         emit(Resource.Loading)
         try {
             val response = pokedexService.fetchPokemonInfo(name = name)
