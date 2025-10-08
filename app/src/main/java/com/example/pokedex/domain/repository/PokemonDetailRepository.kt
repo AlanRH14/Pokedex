3c000a0ca51926197cf4b6b93a4499c45d34ec1c
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-08 16:33:22 -0600

    Config module data part. 76.

---
+++ app/src/main/java/com/example/pokedex/domain/repository/PokemonDetailRepository.kt
@@ -0,0 +1,9 @@
+package com.example.pokedex.domain.repository
+
+import com.example.pokedex.domain.models.PokemonDetail
+import com.example.pokedex.utils.Resource
+import kotlinx.coroutines.flow.Flow
+
+interface PokemonDetailRepository {
+    fun fetchPokemonDetail(name: String): Flow<Resource<PokemonDetail>>
+}
