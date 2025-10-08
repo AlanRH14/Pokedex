2f7b4106d16e41241b86de9979cbeb3ea9af9d65
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-08 08:10:31 -0600

    Config module domain part. 31.

--- app/src/main/java/com/example/pokedex/domain/models/Pokemon.kt
+++ app/src/main/java/com/example/pokedex/domain/models/Pokemon.kt
@@ -2,6 +2,6 @@ package com.example.pokedex.domain.models
 
 data class Pokemon(
     val id: Long = 0,
-    val name: String? = null,
-    val url: String? = null
+    val name: String = "",
+    val url: String = ""
 )
