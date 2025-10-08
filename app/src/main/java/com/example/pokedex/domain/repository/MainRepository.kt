b955b27b8d3a1cbe2d473e63b551cf8a69896e1e
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-08 14:00:16 -0600

    Config module data part. 72.

--- app/src/main/java/com/example/pokedex/domain/repository/MainRepository.kt
+++ app/src/main/java/com/example/pokedex/domain/repository/MainRepository.kt
@@ -1,8 +1,9 @@
 package com.example.pokedex.domain.repository
 
 import com.example.pokedex.data.models.PokemonDto
+import com.example.pokedex.utils.Resource
 import kotlinx.coroutines.flow.Flow
 
 interface MainRepository {
-    fun fetchPokemonList(): Flow<List<PokemonDto>>
+    fun fetchPokemonList(): Flow<Resource<List<PokemonDto>>>
 }
