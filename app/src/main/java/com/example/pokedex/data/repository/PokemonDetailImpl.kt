a40bc41d9c24e6cb7a87e30dd52bb331f6d3d387
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-08 16:44:50 -0600

    Config module data part. 77.

--- app/src/main/java/com/example/pokedex/data/repository/PokemonDetailImpl.kt
+++ app/src/main/java/com/example/pokedex/data/repository/PokemonDetailImpl.kt
@@ -1,8 +1,23 @@
 package com.example.pokedex.data.repository
 
 import com.example.pokedex.data.remote.PokedexService
+import com.example.pokedex.domain.models.PokemonDetail
+import com.example.pokedex.domain.repository.PokemonDetailRepository
+import com.example.pokedex.utils.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class PokemonDetailImpl(
-    pokedexService: PokedexService
-) {
+    private val pokedexService: PokedexService
+): PokemonDetailRepository {
+
+    override fun fetchPokemonDetail(name: String): Flow<Resource<PokemonDetail>> = flow {
+        emit(Resource.Loading)
+        try {
+            val response = pokedexService.fetchPokemonInfo(name = name)
+            emit(Resource.Success(data = response))
+        } catch (e: Exception) {
+
+        }
+    }
 }
