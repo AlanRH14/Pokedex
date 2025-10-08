783361ae481d712faf406f2f9423b13f2b36ba6b
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-08 09:27:34 -0600

    Config module domain part. 49.

--- app/src/main/java/com/example/pokedex/domain/models/Sprites.kt
+++ app/src/main/java/com/example/pokedex/domain/models/Sprites.kt
@@ -1,7 +1,5 @@
 package com.example.pokedex.domain.models
 
-import com.example.pokedex.data.models.OtherSprites
-
 data class Sprites(
     val frontDefault: String = "",
     val backDefault: String= "",
