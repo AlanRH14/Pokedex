a037943fc52e5752bf49b565933e86a705f50b8b
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-08 16:45:39 -0600

    Config module data part. 79.

--- app/src/main/java/com/example/pokedex/data/repository/DetailRepositoryImpl.kt
+++
@@ -1,8 +0,0 @@
-package com.example.pokedex.data.repository
-
-import com.example.pokedex.data.remote.PokedexService
-
-class DetailRepositoryImpl(
-    private val pokedexService: PokedexService
-) {
-}
