10daba273629cf832cb33d77a97be359420f8046
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-08 13:26:36 -0600

    Config module data part. 63.

--- app/src/main/java/com/example/pokedex/data/models/PokemonResponse.kt
+++ app/src/main/java/com/example/pokedex/data/models/PokemonResponse.kt
@@ -2,5 +2,7 @@ package com.example.pokedex.data.models
 
 data class PokemonResponse(
     val count: Long? = null,
-    val next: String? = null
+    val next: String? = null,
+    val previous: String? = null,
+    val results: List<PokemonDto>? = null
 )
