0d6b79b413bd427f31401925365458552c21064b
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-09 13:42:16 -0600

    Config module data database part. 11.

--- app/src/main/java/com/example/pokedex/data/local/PokedexDatabase.kt
+++ app/src/main/java/com/example/pokedex/data/local/PokedexDatabase.kt
@@ -3,7 +3,10 @@ package com.example.pokedex.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [PokemonEntity::class, PokemonDetailEntity::class], version = 1)
+@Database(
+    entities = [PokemonEntity::class, PokemonDetailEntity::class],
+    version = 1
+)
 abstract class PokedexDatabase : RoomDatabase() {
 
     abstract fun pokemonsDao(): PokemonsDao
