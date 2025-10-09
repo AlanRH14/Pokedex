494d322b526b83b121115420e25251880fb6d146
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-09 12:54:48 -0600

    Config module data database part. 3.

--- app/src/main/java/com/example/pokedex/data/local/PokemonDetailEntity.kt
+++ app/src/main/java/com/example/pokedex/data/local/PokemonDetailEntity.kt
@@ -2,4 +2,9 @@ package com.example.pokedex.data.local
 
 data class PokemonDetailEntity(
     val id: Long,
+    val name: String,
+    val height: String,
+    val weight: String,
+    val experience: String,
+    val sprites: String,
 )
