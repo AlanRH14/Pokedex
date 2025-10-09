9ef3091d0f9ae2ab88c939238ed3cd02726bbb01
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-09 17:18:26 -0600

    Config module data database part. 28.

--- app/src/main/java/com/example/pokedex/data/local/DatabaseConverter.kt
+++ app/src/main/java/com/example/pokedex/data/local/DatabaseConverter.kt
@@ -11,48 +11,48 @@ class DatabaseConverter {
 
     @TypeConverter
     fun converterSpritesToString(sprites: Sprites?): String {
-        return if (sprites == null) "" else json.encodeToString(Sprites.serializer(), sprites)
+        return if (sprites == null) "null" else json.encodeToString(Sprites.serializer(), sprites)
     }
 
     @TypeConverter
-    fun converterStringToSprints(sprites: String): Sprites {
-        return if (sprites.isEmpty())
-            Sprites()
+    fun converterStringToSprints(sprites: String): Sprites? {
+        return if (sprites == "null")
+            null
         else
             json.decodeFromString(Sprites.serializer(), sprites)
     }
 
     @TypeConverter
     fun converterTypesToString(types: List<String>?): String {
-        return if (types.isNullOrEmpty()) "" else json.encodeToString(types)
+        return if (types.isNullOrEmpty()) "[]" else json.encodeToString(types)
     }
 
     @TypeConverter
     fun converterStringToTypes(types: String): List<String> {
-        return if (types.isEmpty()) emptyList() else json.decodeFromString(types)
+        return if (types.isBlank() || types == "[]") emptyList() else json.decodeFromString(types)
     }
 
     @TypeConverter
     fun converterStatsToString(stats: List<Stat>?): String {
         return if (stats.isNullOrEmpty())
-            ""
+            "[]"
         else
             json.encodeToString(ListSerializer(Stat.serializer()), stats)
     }
 
     @TypeConverter
     fun converterStringToStats(stats: String): List<Stat> {
-        return if(stats.isEmpty()) emptyList()
+        return if(stats.isBlank() || stats == "[]") emptyList()
         else json.decodeFromString(ListSerializer(Stat.serializer()),stats)
     }
 
     @TypeConverter
     fun converterAbilitiesToString(abilities: List<String>?): String {
-        return if (abilities.isNullOrEmpty()) "" else json.encodeToString(abilities)
+        return if (abilities.isNullOrEmpty()) "[]" else json.encodeToString(abilities)
     }
 
     @TypeConverter
     fun converterStringToAbilities(abilities: String): List<String> {
-        return if (abilities.isEmpty()) emptyList() else json.decodeFromString(abilities)
+        return if (abilities.isBlank() || abilities == "[]") emptyList() else json.decodeFromString(abilities)
     }
 }
