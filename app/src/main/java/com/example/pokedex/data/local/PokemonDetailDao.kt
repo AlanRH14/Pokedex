04ecfec9f0ab511699c0e7237b28489efa9b28ce
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-09 13:41:02 -0600

    Config module data database part. 9.

--- app/src/main/java/com/example/pokedex/data/local/PokemonDetailDao.kt
+++ app/src/main/java/com/example/pokedex/data/local/PokemonDetailDao.kt
@@ -3,10 +3,15 @@ package com.example.pokedex.data.local
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PokemonDetailDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertPokemonDetail(pokemonDetail: PokemonDetailEntity)
+
+    @Query("SELECT * FROM PokemonDetailEntity WHERE name = name")
+    fun getPokemonDetail(name: String): Flow<PokemonDetailEntity>
 }
