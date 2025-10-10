08ef33e4ecdb119c5fb068d7eb1b42b8fe6021e1
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-10 12:49:59 -0600

    Config module data mappers part. 4.

--- app/src/main/java/com/example/pokedex/data/PokemonMapperImpl.kt
+++ app/src/main/java/com/example/pokedex/data/PokemonMapperImpl.kt
@@ -1,7 +1,6 @@
 package com.example.pokedex.data
 
 import com.example.pokedex.common.ApiMapper
-import com.example.pokedex.data.models.PokemonDto
 import com.example.pokedex.data.models.PokemonResponse
 import com.example.pokedex.domain.models.Pokemon
 import com.example.pokedex.utils.Constants.BAR_URL_IMAGE
