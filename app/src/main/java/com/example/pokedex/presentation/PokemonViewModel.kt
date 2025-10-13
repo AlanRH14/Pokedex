d504f5655408b0987abd576a1e7fa1776db7de0a
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-13 12:14:37 -0600

    Config PokemonList Screen part. 16.

--- app/src/main/java/com/example/pokedex/presentation/PokemonViewModel.kt
+++ app/src/main/java/com/example/pokedex/presentation/PokemonViewModel.kt
@@ -1,18 +1,23 @@
 package com.example.pokedex.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pokedex.domain.repository.MainRepository
+import com.example.pokedex.utils.Resource
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class PokemonViewModel(
     private val pokemonRepository: MainRepository
-): ViewModel() {
+) : ViewModel() {
 
     private val _state = MutableStateFlow(PokemonState())
-    val  state = _state.asStateFlow()
+    val state = _state.asStateFlow()
 
     private val _effect = MutableSharedFlow<PokemonEffect>()
     val effect = _effect.asSharedFlow()
@@ -22,6 +27,25 @@ class PokemonViewModel(
             is PokemonUIEvent.OnGetPokemonList -> Unit
 
             is PokemonUIEvent.OnClickPokemonDetail -> Unit
+        }
+    }
+
+    private fun getPokemons() {
+        viewModelScope.launch(Dispatchers.IO) {
+            pokemonRepository.fetchPokemonList().collect { result ->
+                when (result) {
+                    is Resource.Loading -> _state.update { it.copy(isLoading = true) }
+
+                    is Resource.Success -> _state.update {
+                        it.copy(
+                            pokemonList = result.data,
+                            isLoading = false
+                        )
+                    }
+
+                    is Resource.Error -> _state.update { it.copy(errorMessage = result.message) }
+                }
+            }
         }
     }
 }
