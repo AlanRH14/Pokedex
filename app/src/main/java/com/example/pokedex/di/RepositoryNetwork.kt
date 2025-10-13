b31885b27ffd60670ac1e68d879f2697608250f8
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-13 07:59:08 -0600

    Config module data mappers part. 17.

--- app/src/main/java/com/example/pokedex/di/RepositoryNetwork.kt
+++ app/src/main/java/com/example/pokedex/di/RepositoryNetwork.kt
@@ -1,8 +1,10 @@
 package com.example.pokedex.di
 
 import com.example.pokedex.data.repository.MainRepositoryImpl
+import com.example.pokedex.data.repository.PokemonDetailImpl
 import org.koin.dsl.module
 
 val repositoryNetwork = module {
     single { MainRepositoryImpl(pokedexService = get(), pokemonMapper = get()) }
+    single { PokemonDetailImpl(pokedexService = get()) }
 }
