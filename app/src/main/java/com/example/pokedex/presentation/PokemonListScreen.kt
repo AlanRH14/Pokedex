b3b70e62c0f36602db68075462c24ef9d5f71c1e
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-13 12:18:15 -0600

    Config PokemonList Screen part. 20.

--- app/src/main/java/com/example/pokedex/presentation/PokemonListScreen.kt
+++ app/src/main/java/com/example/pokedex/presentation/PokemonListScreen.kt
@@ -4,11 +4,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun PokemonListScreen(
-    modifier: Modifier
+    modifier: Modifier,
+    viewModel: PokemonViewModel = koinViewModel()
 ) {
+
     Column(
         modifier = modifier
             .fillMaxSize()
