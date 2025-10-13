aa16387172f103f88b1a9d1f4f648b30faace45e
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-13 13:29:25 -0600

    Config PokemonList Screen part. 42.

--- app/src/main/java/com/example/pokedex/di/ApiMapperModule.kt
+++ app/src/main/java/com/example/pokedex/di/ApiMapperModule.kt
@@ -3,12 +3,14 @@ package com.example.pokedex.di
 import com.example.pokedex.common.ApiMapper
 import com.example.pokedex.data.PokemonDetailMapperImpl
 import com.example.pokedex.data.PokemonMapperImpl
+import com.example.pokedex.data.models.PokemonDetailDto
 import com.example.pokedex.data.models.PokemonResponse
 import com.example.pokedex.domain.models.Pokemon
+import com.example.pokedex.domain.models.PokemonDetail
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val apiMapperModule = module {
     single<ApiMapper<PokemonResponse, List<Pokemon>>>(named("PokemonMapperImpl")) { PokemonMapperImpl() }
-    single { PokemonDetailMapperImpl() }
+    single<ApiMapper<PokemonDetailDto, PokemonDetail>>(named("PokemonDetailMapperImpl")) { PokemonDetailMapperImpl() }
 }
