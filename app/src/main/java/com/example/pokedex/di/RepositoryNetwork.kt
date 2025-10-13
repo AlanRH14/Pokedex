0645bdf238c14b41f9dc470c9e2e5733108be8ed
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-13 13:37:59 -0600

    Config PokemonList Screen part. 44.

--- app/src/main/java/com/example/pokedex/di/RepositoryNetwork.kt
+++ app/src/main/java/com/example/pokedex/di/RepositoryNetwork.kt
@@ -3,10 +3,11 @@ package com.example.pokedex.di
 import com.example.pokedex.data.repository.MainRepositoryImpl
 import com.example.pokedex.data.repository.PokemonDetailImpl
 import com.example.pokedex.domain.repository.MainRepository
+import com.example.pokedex.domain.repository.PokemonDetailRepository
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val repositoryNetwork = module {
-    single<MainRepository> { MainRepositoryImpl(pokedexService = get(named("PokemonMapperImpl")), pokemonMapper = get()) }
-    single { PokemonDetailImpl(pokedexService = get(named("PokemonDetailMapperImpl")), pokemonDetailMapper = get()) }
+    single<MainRepository> { MainRepositoryImpl(pokedexService = get(), pokemonMapper = get(named("PokemonMapperImpl"))) }
+    single<PokemonDetailRepository> { PokemonDetailImpl(pokedexService = get(), pokemonDetailMapper = get(named("PokemonDetailMapperImpl"))) }
 }
