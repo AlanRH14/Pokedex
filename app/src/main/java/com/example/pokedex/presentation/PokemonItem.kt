94702021723957b64dbda326c6ebdaa835722b37
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-13 16:42:48 -0600

    Config PokemonList Screen part. 52.

--- app/src/main/java/com/example/pokedex/presentation/PokemonItem.kt
+++ app/src/main/java/com/example/pokedex/presentation/PokemonItem.kt
@@ -1,9 +1,11 @@
 package com.example.pokedex.presentation
 
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -14,8 +16,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import coil3.request.crossfade
 import com.example.pokedex.domain.models.Pokemon
 
 @Composable
@@ -23,6 +29,10 @@ fun PokemonItem(
     pokemon: Pokemon,
     onEvent: (PokemonUIEvent) -> Unit
 ) {
+    val imgRequest = ImageRequest.Builder(LocalContext.current)
+        .data(pokemon.url)
+        .crossfade(true)
+        .build()
 
     Card(
         modifier = Modifier,
@@ -40,9 +50,12 @@ fun PokemonItem(
                     .fillMaxWidth()
                     .aspectRatio(1.2F)
                     .fillMaxHeight(),
-                model = pokemon.url,
+                model = imgRequest,
+                onError = {
+                    Log.d("LordMiau", "Error Image: ${it.result.throwable.message}")
+                },
                 contentDescription = "Image ${pokemon.name}",
-                contentScale = ContentScale.Fit
+                contentScale = ContentScale.Crop
             )
 
             Text(
