97ccd43dedbdb763eb1ac8008652b204f95a8d9e
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-13 12:02:58 -0600

    Config PokemonList Screen part. 6.

--- app/src/main/java/com/example/pokedex/presentation/PokemonState.kt
+++ app/src/main/java/com/example/pokedex/presentation/PokemonState.kt
@@ -4,5 +4,6 @@ import com.example.pokedex.domain.models.Pokemon
 
 data class PokemonState(
     val isLoading : Boolean = true,
-    val pokemonList: List<Pokemon> = emptyList()
+    val pokemonList: List<Pokemon> = emptyList(),
+    val errorMessage: String? = null
 )
