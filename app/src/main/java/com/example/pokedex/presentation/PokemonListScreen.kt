ea3b001ee31481ad5a357f290db3b7656388257c
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-13 16:15:07 -0600

    Config PokemonList Screen part. 46.

--- app/src/main/java/com/example/pokedex/presentation/PokemonListScreen.kt
+++ app/src/main/java/com/example/pokedex/presentation/PokemonListScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -51,6 +52,10 @@ fun PokemonListScreen(
                     onEvent = viewModel::onEvent
                 )
             }
+        }
+
+        if (!state.errorMessage.isNullOrEmpty()) {
+            Text(state.errorMessage ?: "")
         }
     }
 }
