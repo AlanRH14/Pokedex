a08d91957b8ebde9a039869e1f9d01693d33fb55
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-13 08:01:24 -0600

    Config module data mappers part. 20.

--- app/src/main/java/com/example/pokedex/domain/repository/PokemonDetailRepository.kt
+++ app/src/main/java/com/example/pokedex/domain/repository/PokemonDetailRepository.kt
@@ -1,9 +1,10 @@
 package com.example.pokedex.domain.repository
 
 import com.example.pokedex.data.models.PokemonDetailDto
+import com.example.pokedex.domain.models.PokemonDetail
 import com.example.pokedex.utils.Resource
 import kotlinx.coroutines.flow.Flow
 
 interface PokemonDetailRepository {
-    fun fetchPokemonDetail(name: String): Flow<Resource<PokemonDetailDto>>
+    fun fetchPokemonDetail(name: String): Flow<Resource<PokemonDetail>>
 }
