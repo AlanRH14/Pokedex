50935ffec3b83179bf9cad1f7ffed0b1d1c3dc11
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-13 12:37:26 -0600

    Config PokemonList Screen part. 26.

---
+++ app/src/main/java/com/example/pokedex/presentation/PokemonItem.kt
@@ -0,0 +1,12 @@
+package com.example.pokedex.presentation
+
+import androidx.compose.runtime.Composable
+import com.example.pokedex.domain.models.Pokemon
+
+@Composable
+fun PokemonItem(
+    pokemon: Pokemon,
+    onEvent: (PokemonUIEvent) -> Unit
+) {
+
+}
