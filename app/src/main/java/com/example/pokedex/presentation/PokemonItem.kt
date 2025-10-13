c1ec3377c883f69241dd8155d13346b0252e6cc8
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-13 12:46:43 -0600

    Config PokemonList Screen part. 27.

--- app/src/main/java/com/example/pokedex/presentation/PokemonItem.kt
+++ app/src/main/java/com/example/pokedex/presentation/PokemonItem.kt
@@ -1,6 +1,12 @@
 package com.example.pokedex.presentation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.example.pokedex.domain.models.Pokemon
 
 @Composable
@@ -9,4 +15,14 @@ fun PokemonItem(
     onEvent: (PokemonUIEvent) -> Unit
 ) {
 
+    Card(
+        modifier = Modifier,
+        colors = CardDefaults.cardColors(
+            containerColor = Color.Transparent,
+            contentColor = MaterialTheme.colorScheme.onBackground
+        ),
+        onClick = { onEvent(PokemonUIEvent.OnClickPokemonDetail(pokemonName = pokemon.name)) }
+    ) {
+
+    }
 }
