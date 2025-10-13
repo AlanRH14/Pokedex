c467b35126edb165f08abcaade5f54aecf8b4e7a
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-13 12:33:07 -0600

    Config NavRoute part. 6.

--- app/src/main/java/com/example/pokedex/navigation/NavigationHost.kt
+++ app/src/main/java/com/example/pokedex/navigation/NavigationHost.kt
@@ -1,11 +1,27 @@
 package com.example.pokedex.navigation
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 
 @Composable
 fun NavigationHost(
+    modifier: Modifier,
     navController: NavHostController
 ) {
 
+    NavHost(
+        modifier = modifier
+            .fillMaxSize(),
+        navController = navController,
+        startDestination = NavRoute.PokemonsScreen
+    ) {
+
+        composable<NavRoute.PokemonsScreen> {
+
+        }
+    }
 }
