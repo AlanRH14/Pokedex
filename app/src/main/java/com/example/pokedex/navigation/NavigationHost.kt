a55a7e3e77885ea89db748cff2c21d383ad15424
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-13 12:34:36 -0600

    Config NavRoute part. 7.

--- app/src/main/java/com/example/pokedex/navigation/NavigationHost.kt
+++ app/src/main/java/com/example/pokedex/navigation/NavigationHost.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.pokedex.presentation.PokemonListScreen
 
 @Composable
 fun NavigationHost(
@@ -21,7 +22,10 @@ fun NavigationHost(
     ) {
 
         composable<NavRoute.PokemonsScreen> {
-
+            PokemonListScreen(
+                modifier = modifier,
+                navController = navController,
+            )
         }
     }
 }
