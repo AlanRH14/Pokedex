99f5025a87c9489fb092a067c039d025cae2779a
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-13 12:03:41 -0600

    Config PokemonList Screen part. 7.

--- app/src/main/java/com/example/pokedex/presentation/PokemonViewModel.kt
+++ app/src/main/java/com/example/pokedex/presentation/PokemonViewModel.kt
@@ -2,10 +2,13 @@ package com.example.pokedex.presentation
 
 import androidx.lifecycle.ViewModel
 import com.example.pokedex.domain.repository.MainRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class PokemonViewModel(
     private val pokemonRepository: MainRepository
 ): ViewModel() {
 
-
+    private val _state = MutableStateFlow(PokemonState())
+    val  state = _state.asStateFlow()
 }
