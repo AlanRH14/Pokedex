0fdc0659329930790ecdce977912d1cde0498b44
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-14 11:55:32 -0600

    Config Pokemon Detail Screen part. 14.

---
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonDetailUIEvent.kt
@@ -0,0 +1,5 @@
+package com.example.pokedex.presentation.screens.pokemon_details
+
+sealed interface PokemonDetailUIEvent {
+    data class GetPokemonDetail(val name: String): PokemonDetailUIEvent
+}
