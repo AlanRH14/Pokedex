33596a0db4326184d20a44449cc641d09a168859
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-14 12:36:22 -0600

    Config Pokemon Detail Screen part. 30.

--- app/src/main/java/com/example/pokedex/di/AppModule.kt
+++ app/src/main/java/com/example/pokedex/di/AppModule.kt
@@ -1,9 +1,11 @@
 package com.example.pokedex.di
 
 import com.example.pokedex.presentation.screens.pokedex.PokemonViewModel
+import com.example.pokedex.presentation.screens.pokemon_details.PokemonDetailViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
     viewModel { PokemonViewModel(pokemonRepository = get()) }
+    viewModel { PokemonDetailViewModel(pokemonDetail = get()) }
 }
