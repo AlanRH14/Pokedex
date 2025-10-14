480f76c3160724caa5ba4a1ef43eb474463e90f3
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-14 09:42:58 -0600

    Config PokemonList Screen part. 58.

--- app/src/main/java/com/example/pokedex/presentation/PokemonItem.kt
+++ app/src/main/java/com/example/pokedex/presentation/PokemonItem.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
