bffa6d1a70255cf5ebfba0b2052ffa506f06b7bd
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-14 10:42:34 -0600

    Config PokemonList Screen part. 77.

--- app/src/main/java/com/example/pokedex/presentation/screens/pokedex/components/PokemonItem.kt
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokedex/components/PokemonItem.kt
@@ -1,4 +1,4 @@
-package com.example.pokedex.presentation.screens.pokedex
+package com.example.pokedex.presentation.screens.pokedex.components
 
 import android.util.Log
 import androidx.compose.foundation.background
