8ee6dc4aa4c46916e28c6cd460573bfc3e7b66fe
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-14 12:26:33 -0600

    Config Pokemon Detail Screen part. 24.

--- app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonDetailsScreen.kt
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonDetailsScreen.kt
@@ -3,7 +3,10 @@ package com.example.pokedex.presentation.screens.pokemon_details
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import org.koin.androidx.compose.koinViewModel
 
@@ -14,6 +17,13 @@ fun PokemonDetailsScreen(
     viewModel: PokemonDetailViewModel = koinViewModel(),
     navController: NavHostController,
 ) {
+
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    LaunchedEffect(key1 = true) {
+        viewModel.onEvent(PokemonDetailUIEvent.GetPokemonDetail(pokemonName = pokemonName))
+    }
+
     Column(
         modifier = modifier.fillMaxSize()
     ) {
