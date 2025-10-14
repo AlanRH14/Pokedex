f55a80fbf85b9580b6739814a4703f1e18c758e4
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-14 10:10:15 -0600

    Config Pokedex Colors part. 5.

--- app/src/main/java/com/example/pokedex/ui/theme/Theme.kt
+++ app/src/main/java/com/example/pokedex/ui/theme/Theme.kt
@@ -47,7 +47,6 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun PokedexTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
