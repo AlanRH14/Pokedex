59a2aaddd1bdbf2cc4677b25f681c7793f1db3e7
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-14 11:01:13 -0600

    Config Pokemon Detail Screen part. 12.

---
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonDetailViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.pokedex.presentation.screens.pokemon_details
+
+import com.example.pokedex.domain.repository.PokemonDetailRepository
+
+class PokemonDetailViewModel(
+    private val pokemonDetail: PokemonDetailRepository
+) {
+
+}
