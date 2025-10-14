57d5746672a069e40d615ff66b446c4fac917c38
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-14 12:55:31 -0600

    Config Pokemon Detail mapper error part. 6.

--- app/src/main/java/com/example/pokedex/utils/StringUtils.kt
+++ app/src/main/java/com/example/pokedex/utils/StringUtils.kt
@@ -2,4 +2,12 @@ package com.example.pokedex.utils
 
 object StringUtils {
 
+    fun String?.capitalized(): String {
+        return this?.replaceFirstChar {
+            if (it.isLowerCase())
+                it.uppercase()
+            else
+                it.toString()
+        } ?: ""
+    }
 }
