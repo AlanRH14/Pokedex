bb021e9956b1154039b9da66b76094f879d8a0fe
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-14 13:14:38 -0600

    Config Pokemon Detail Screen part. 40.

--- app/src/main/java/com/example/pokedex/data/mappers/PokemonDetailMapperImpl.kt
+++ app/src/main/java/com/example/pokedex/data/mappers/PokemonDetailMapperImpl.kt
@@ -5,15 +5,15 @@ import com.example.pokedex.data.models.PokemonDetailDto
 import com.example.pokedex.domain.models.PokemonDetail
 import com.example.pokedex.domain.models.Sprites
 import com.example.pokedex.domain.models.Stat
-import com.example.pokedex.utils.Constants.BAR_URL_IMAGE
 import com.example.pokedex.utils.StringUtils.capitalized
+import com.example.pokedex.utils.StringUtils.formatPokemonID
 import com.example.pokedex.utils.StringUtils.formatPokemonImageURL
 
 class PokemonDetailMapperImpl : ApiMapper<PokemonDetailDto, PokemonDetail> {
 
     override fun mapperToDomain(dto: PokemonDetailDto): PokemonDetail {
         return PokemonDetail(
-            id = formatPokemonID(id = dto.id),
+            id = dto.id.formatPokemonID(),
             name = dto.name.capitalized(),
             url = dto.id.formatPokemonImageURL(),
             height = dto.height ?: 0,
