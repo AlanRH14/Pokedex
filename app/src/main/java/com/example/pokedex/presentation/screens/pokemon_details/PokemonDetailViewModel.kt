4dc953c4dfc0bbc334d05b6a7b31866e7e39daaa
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-14 12:01:44 -0600

    Config Pokemon Detail Screen part. 16.

--- app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonDetailViewModel.kt
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonDetailViewModel.kt
@@ -1,9 +1,13 @@
 package com.example.pokedex.presentation.screens.pokemon_details
 
 import com.example.pokedex.domain.repository.PokemonDetailRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class PokemonDetailViewModel(
     private val pokemonDetail: PokemonDetailRepository
 ) {
 
+    private val _state = MutableStateFlow(PokemonDetailState())
+    val state = _state.asStateFlow()
 }
