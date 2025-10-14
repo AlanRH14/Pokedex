e9a54a63b3da9092cea72cc2363f21c49f4edac1
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-14 10:59:25 -0600

    Config Pokemon Detail Screen part. 9.

--- app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonDetailsScreen.kt
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonDetailsScreen.kt
@@ -1,8 +1,21 @@
 package com.example.pokedex.presentation.screens.pokemon_details
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import com.example.pokedex.domain.models.PokemonDetail
 
 @Composable
-fun PokemonDetailsScreen() {
+fun PokemonDetailsScreen(
+    modifier: Modifier = Modifier,
+    pokemonDetail: PokemonDetail,
+    navController: NavHostController,
+) {
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
 
+    }
 }
