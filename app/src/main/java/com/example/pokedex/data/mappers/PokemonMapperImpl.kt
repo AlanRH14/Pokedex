456d1f795819f50a4bc0ffa071cf8c29dd4aecc3
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-14 12:57:40 -0600

    Config Pokemon Detail mapper error part. 9.

--- app/src/main/java/com/example/pokedex/data/mappers/PokemonMapperImpl.kt
+++ app/src/main/java/com/example/pokedex/data/mappers/PokemonMapperImpl.kt
@@ -4,6 +4,7 @@ import com.example.pokedex.common.ApiMapper
 import com.example.pokedex.data.models.PokemonResponse
 import com.example.pokedex.domain.models.Pokemon
 import com.example.pokedex.utils.Constants
+import com.example.pokedex.utils.StringUtils.capitalized
 import java.util.Locale
 
 class PokemonMapperImpl : ApiMapper<PokemonResponse, List<Pokemon>> {
@@ -12,7 +13,7 @@ class PokemonMapperImpl : ApiMapper<PokemonResponse, List<Pokemon>> {
         return dto.results?.mapIndexed { index, pokemon ->
             Pokemon(
                 id = formatPokemonID(index),
-                name = pokemon.name?.capitalized() ?: "",
+                name = pokemon.name.capitalized(),
                 url = "${Constants.BAR_URL_IMAGE}${index + 1}.png"
             )
         } ?: emptyList()
@@ -23,15 +24,6 @@ class PokemonMapperImpl : ApiMapper<PokemonResponse, List<Pokemon>> {
             1 -> "#00$id"
             2 -> "#0$id"
             else -> "#$id"
-        }
-    }
-
-    private fun String.capitalized(): String {
-        return this.replaceFirstChar {
-            if (it.isLowerCase())
-                it.titlecase(Locale.getDefault())
-            else
-                it.toString()
         }
     }
 }
