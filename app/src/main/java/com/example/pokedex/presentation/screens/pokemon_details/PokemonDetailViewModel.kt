b30e960ca47180118e393040b46d0b03935af5b8
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-14 12:08:29 -0600

    Config Pokemon Detail Screen part. 18.

--- app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonDetailViewModel.kt
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonDetailViewModel.kt
@@ -1,18 +1,44 @@
 package com.example.pokedex.presentation.screens.pokemon_details
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pokedex.domain.repository.PokemonDetailRepository
+import com.example.pokedex.utils.Resource
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class PokemonDetailViewModel(
     private val pokemonDetail: PokemonDetailRepository
-) {
+) : ViewModel() {
 
     private val _state = MutableStateFlow(PokemonDetailState())
     val state = _state.asStateFlow()
 
     private val _effect = MutableSharedFlow<PokemonDetailEffect>()
     val effect = _effect.asSharedFlow()
+
+    fun onEvent(event: PokemonDetailUIEvent) {
+        when (event) {
+            is PokemonDetailUIEvent.GetPokemonDetail -> getPokemonDetail(pokemonName = event.name)
+        }
+    }
+
+    private fun getPokemonDetail(pokemonName: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            pokemonDetail.fetchPokemonDetail(name = pokemonName).collect { result ->
+                when (result) {
+                    is Resource.Loading -> _state.update { it.copy(isLoading = true) }
+
+                    is Resource.Success -> _state.update { it.copy(isLoading = false) }
+
+                    is Resource.Error -> _state.update { it.copy(isLoading = false) }
+                }
+            }
+        }
+    }
 }
