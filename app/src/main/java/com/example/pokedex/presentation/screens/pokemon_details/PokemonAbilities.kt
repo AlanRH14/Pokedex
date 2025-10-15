25f61a1c750b62f6afa91cf8a90a164eec08344b
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-15 12:56:01 -0600

    Config Pokemon Detail Screen part. 77.

--- app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonAbilities.kt
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonAbilities.kt
@@ -1,12 +1,10 @@
 package com.example.pokedex.presentation.screens.pokemon_details
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
