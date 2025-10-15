6a2cb75e49bd32fb91bb1237568fdbf53783f452
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-15 12:31:00 -0600

    Config Pokemon Detail Screen part. 72.

---
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonAbilityItem.kt
@@ -0,0 +1,12 @@
+package com.example.pokedex.presentation.screens.pokemon_details
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.example.pokedex.data.models.Ability
+
+@Composable
+fun PokemonAbilityItem(abilities: Ability) {
+    Text(
+        ""
+    )
+}
