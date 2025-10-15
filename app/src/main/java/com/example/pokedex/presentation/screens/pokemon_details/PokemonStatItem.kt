1a306bba261ff8e67b1a83806a64a20e538d6117
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-15 16:56:00 -0600

    Config Pokemon Detail PokemonStats config part. 1.

---
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonStatItem.kt
@@ -0,0 +1,63 @@
+package com.example.pokedex.presentation.screens.pokemon_details
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import com.example.pokedex.domain.models.Stat
+import kotlin.math.roundToInt
+
+@Composable
+fun PokemonStatItem(
+    modifier: Modifier = Modifier,
+    stat: Stat
+) {
+
+    val animationProgress = remember {
+        Animatable(initialValue = 0F)
+    }
+
+    LaunchedEffect(key1 = Unit) {
+        animationProgress.animateTo(
+            targetValue = 1F,
+            animationSpec = tween(
+                durationMillis = 8 * stat.baseStat.toInt(),
+                easing = LinearEasing
+            )
+        )
+    }
+
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+
+        Text(
+            modifier = Modifier.weight(.3F),
+            text = stat.stat,
+            color = MaterialTheme.colorScheme.outline.copy(0.8F),
+            style = MaterialTheme.typography.bodyLarge.copy(
+                fontWeight = FontWeight.Bold
+            )
+        )
+
+        Text(
+            modifier = Modifier.weight(.2F),
+            text = "${(stat.baseStat.toInt() * animationProgress.value).roundToInt()}",
+            color = MaterialTheme.colorScheme.onBackground,
+            style = MaterialTheme.typography.bodyLarge.copy(
+                fontWeight = FontWeight.Bold
+            )
+        )
+
+        val progress = stat.baseStat.toFloat()
+    }
+}
