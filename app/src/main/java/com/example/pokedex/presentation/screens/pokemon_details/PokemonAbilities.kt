e05c4d160845d482cdaacacfa0f667609551053c
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-15 12:50:23 -0600

    Config Pokemon Detail Screen part. 74.

---
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonAbilities.kt
@@ -0,0 +1,22 @@
+package com.example.pokedex.presentation.screens.pokemon_details
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun PokemonAbilities(
+    modifier: Modifier = Modifier,
+    abilities: List<String>
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        items(abilities) { ability ->
+            PokemonAbilityItem(ability = ability)
+        }
+    }
+}
