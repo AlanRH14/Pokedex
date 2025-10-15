5727f557037da63fd83ba70fb01b4b9791d50ff7
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-15 13:51:42 -0600

    Config Pokemon Detail Screen part. 91.

--- app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonInfo.kt
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonInfo.kt
@@ -2,10 +2,14 @@ package com.example.pokedex.presentation.screens.pokemon_details
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.pokedex.R
@@ -16,7 +20,17 @@ fun PokemonInfo(
     height: Long,
 ) {
 
-    Card {
+    Card(
+        modifier = Modifier
+            .padding(
+                horizontal = 12.dp,
+                vertical = 16.dp
+            ),
+        shape = MaterialTheme.shapes.large,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.outline.copy(.2F),
+        )
+    ) {
         Row(
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically,
--- app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonInfoItem.kt
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonInfoItem.kt
@@ -3,20 +3,29 @@ package com.example.pokedex.presentation.screens.pokemon_details
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun PokemonInfoItem(
+    modifier: Modifier = Modifier,
     icon: Painter,
     textInfo: String,
     title: String,
     contentDescription: String
 ) {
     Column(
+        modifier = modifier.padding(all = 16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
@@ -25,13 +34,27 @@ fun PokemonInfoItem(
             verticalAlignment = Alignment.CenterVertically
         ) {
             Icon(
+                modifier = Modifier.padding(horizontal = 4.dp),
                 painter = icon,
                 contentDescription = contentDescription,
             )
 
-            Text(textInfo)
+            Text(
+                text = textInfo,
+                style = MaterialTheme.typography.bodyLarge.copy(
+                    fontWeight = FontWeight.Bold
+                ),
+                textAlign = TextAlign.Center,
+            )
         }
 
-        Text(title)
+        Text(
+            modifier = Modifier
+                .alpha(0.5F)
+                .padding(4.dp),
+            text = title,
+            style = MaterialTheme.typography.bodyMedium,
+            textAlign = TextAlign.Center
+        )
     }
 }
