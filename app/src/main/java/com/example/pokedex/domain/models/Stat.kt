dc1d85e697746a91bdb2dc6aa6abb43b507f9f0b
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-15 17:09:22 -0600

    Config Pokemon Detail PokemonStats config part. 2.

--- app/src/main/java/com/example/pokedex/domain/models/Stat.kt
+++ app/src/main/java/com/example/pokedex/domain/models/Stat.kt
@@ -4,6 +4,37 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Stat(
-    val baseStat: Long = 0,
+    val baseStat: Int = 0,
     val stat: String = ""
-)
+) {
+    val maxValue: Int
+        get() = when (stat) {
+            "hp" -> MAX_HP
+            "attack" -> MAX_ATTACK
+            "defense" -> MAX_DEFENCE
+            "special-attack" -> MAX_SP_ATTACK
+            "special-defense" -> MAX_SP_DEFENCE
+            "speed" -> MAX_SPEED
+            else -> baseStat
+        }
+
+    val name: String
+        get() = when (stat) {
+            "hp" -> "HP"
+            "attack" -> "Attack"
+            "defense" -> "Defense"
+            "special-attack" -> "Sp. Attack"
+            "special-defense" -> "Sp. Defense"
+            "speed" -> "Speed"
+            else -> ""
+        }
+
+    companion object {
+        const val MAX_HP = 255
+        const val MAX_ATTACK = 190
+        const val MAX_DEFENCE = 230
+        const val MAX_SP_ATTACK = 200
+        const val MAX_SP_DEFENCE = 230
+        const val MAX_SPEED = 180
+    }
+}
