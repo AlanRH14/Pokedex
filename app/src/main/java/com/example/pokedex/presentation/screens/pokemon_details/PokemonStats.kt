6047efedb613fd0c1229c2433ecfa1af22888758
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-15 17:37:21 -0600

    Config Pokemon Detail PokemonStats config part. 8.

--- app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonStats.kt
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonStats.kt
@@ -2,9 +2,11 @@ package com.example.pokedex.presentation.screens.pokemon_details
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.key
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.pokedex.domain.models.Stat
 
 @Composable
@@ -14,7 +16,11 @@ fun PokemonStats(
 ) {
     Column(
         modifier = modifier
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .padding(
+                horizontal = 32.dp,
+                vertical = 16.dp
+            ),
     ) {
         stats.forEach { stat ->
             key(stat.name) {
