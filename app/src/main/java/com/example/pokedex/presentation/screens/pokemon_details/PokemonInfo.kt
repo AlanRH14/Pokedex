6d8de81082e2f5daeffb7a88504243ab1ca543d3
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-15 13:30:06 -0600

    Config Pokemon Detail Screen part. 88.

--- app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonInfo.kt
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonInfo.kt
@@ -1,10 +1,46 @@
 package com.example.pokedex.presentation.screens.pokemon_details
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.pokedex.R
 
 @Composable
 fun PokemonInfo(
-
+    weight: Long,
+    height: Long,
 ) {
 
+    Card {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            PokemonInfoItem(
+                icon = painterResource(R.drawable.ic_favorite_border),
+                textInfo = "$weight kg",
+                title = "Weight",
+                contentDescription = "Icon Weight",
+            )
+
+            VerticalDivider(modifier = Modifier.fillMaxHeight(), thickness = 1.dp)
+
+            PokemonInfoItem(
+                icon = painterResource(R.drawable.ic_favorite_border),
+                textInfo = "$height m",
+                title = "Height",
+                contentDescription = "Icon Height"
+            )
+        }
+    }
 }
---
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonInfoItem.kt
@@ -0,0 +1,29 @@
+package com.example.pokedex.presentation.screens.pokemon_details
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+
+@Composable
+fun PokemonInfoItem(
+    icon: Painter,
+    textInfo: String,
+    title: String,
+    contentDescription: String
+) {
+    Column {
+        Row {
+            Icon(
+                painter = icon,
+                contentDescription = contentDescription,
+            )
+
+            Text(textInfo)
+        }
+
+        Text(title)
+    }
+}
