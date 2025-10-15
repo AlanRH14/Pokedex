a28bb9787d1031a5d74bf7e0f2e9e29f5ff70e80
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-15 17:30:15 -0600

    Config Pokemon Detail PokemonStats config part. 5.

--- app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonStatItem.kt
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonStatItem.kt
@@ -3,7 +3,9 @@ package com.example.pokedex.presentation.screens.pokemon_details
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,7 +13,12 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.example.pokedex.domain.models.Stat
 import com.example.pokedex.ui.theme.Green300
 import com.example.pokedex.ui.theme.Red400
@@ -65,12 +72,33 @@ fun PokemonStatItem(
         val animatedProgress = progress * animationProgress.value
 
         val progressColor = when (progress) {
-            in 0.0 .. 0.2 -> Red400
+            in 0.0F..0.2F -> Red400
 
-            in 0.2 .. 0.5 -> Yellow400
+            in 0.3F..0.5F -> Yellow400
 
             else -> Green300
         }
         val progressTrackColor = MaterialTheme.colorScheme.outline.copy(0.2F)
+
+        Box(
+            modifier = Modifier
+                .weight(0.5F)
+                .height(10.dp)
+                .drawBehind {
+                    drawRoundRect(
+                        color = progressTrackColor,
+                        topLeft = Offset.Zero,
+                        size = size,
+                        cornerRadius = CornerRadius(size.height, size.height)
+                    )
+
+                    drawRoundRect(
+                        color = progressColor,
+                        topLeft = Offset.Zero,
+                        size = Size(width = size.width * animatedProgress, height = size.height),
+                        cornerRadius = CornerRadius(size.height, size.height)
+                    )
+                }
+        )
     }
 }
