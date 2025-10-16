99362811f860736ace84e74c60da408a930b1ff7
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-16 13:08:39 -0600

    Config Pokemon Detail package part. 6.

--- app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/widgets/PokemonInfo.kt
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/widgets/PokemonInfo.kt
@@ -15,7 +15,7 @@ import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.pokedex.R
-import com.example.pokedex.presentation.screens.pokemon_details.PokemonInfoItem
+import com.example.pokedex.presentation.screens.pokemon_details.components.PokemonInfoItem
 
 @Composable
 fun PokemonInfo(
