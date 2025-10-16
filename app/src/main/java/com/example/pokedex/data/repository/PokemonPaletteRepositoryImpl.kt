d389636c3110cffbbc214d058d0d610d061f5c9d
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-16 17:03:51 -0600

    Config Pokemon Item Palette generator config part. 18.

--- app/src/main/java/com/example/pokedex/data/repository/PokemonPaletteRepositoryImpl.kt
+++ app/src/main/java/com/example/pokedex/data/repository/PokemonPaletteRepositoryImpl.kt
@@ -2,11 +2,15 @@ package com.example.pokedex.data.repository
 
 import com.example.pokedex.data.remote.ImageRemoteDataSource
 import com.example.pokedex.data.remote.PaletteDataSource
+import com.example.pokedex.domain.models.PokemonPaletteColors
+import com.example.pokedex.domain.repository.PokemonPaletteRepository
 
 class PokemonPaletteRepositoryImpl(
     private val imageRemoteDataSource: ImageRemoteDataSource,
     private val paletteDataSource: PaletteDataSource
-) {
+): PokemonPaletteRepository {
 
-
+    override suspend fun generatePokemonPalette(pokemonURL: String): PokemonPaletteColors {
+        return
+    }
 }
--- app/src/main/java/com/example/pokedex/domain/repository/PokemonPaletteRepository.kt
+++ app/src/main/java/com/example/pokedex/domain/repository/PokemonPaletteRepository.kt
@@ -1,5 +1,7 @@
 package com.example.pokedex.domain.repository
 
+import com.example.pokedex.domain.models.PokemonPaletteColors
+
 interface PokemonPaletteRepository {
-    suspend fun generatePokemonPalette(pokemonURL: String): PokemonPaletteRepository
+    suspend fun generatePokemonPalette(pokemonURL: String): PokemonPaletteColors
 }
