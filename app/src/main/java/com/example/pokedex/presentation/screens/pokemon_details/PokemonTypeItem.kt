7988f17b84fb0738b7ddf1a6cfebffeed7f2b92c
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-16 12:54:19 -0600

    Config Pokemon Type icons part. 44.

--- app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonTypeItem.kt
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonTypeItem.kt
@@ -1,14 +1,18 @@
 package com.example.pokedex.presentation.screens.pokemon_details
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -28,11 +32,17 @@ fun PokemonTypes(ability: String) {
                 horizontal = 16.dp,
                 vertical = 8.dp
             ),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Icon(
+            modifier = Modifier.size(14.dp),
             painter = painterResource(id = ability.icon),
             contentDescription = "Icon $ability",
         )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
         Text(
             text = ability,
             color = Color.White,
