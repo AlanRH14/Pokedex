fe7e8b9a12c2f0614dd5f2fa5ed66f843a792330
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-16 13:30:38 -0600

    Config Pokemon detail top header part. 2.

--- app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/widgets/PokemonDetailTopBar.kt
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/widgets/PokemonDetailTopBar.kt
@@ -1,8 +1,75 @@
 package com.example.pokedex.presentation.screens.pokemon_details.widgets
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconToggleButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import com.example.pokedex.R
+import com.example.pokedex.presentation.screens.pokemon_details.mvi.PokemonDetailUIEvent
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun PokemonDetailTopBar() {
+fun PokemonDetailTopBar(
 
+) {
+    TopAppBar(
+        title = {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .alpha(8F),
+                text = state.pokemonDetail?.name ?: "Pokemon Empty",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleLarge.copy(
+                    fontWeight = FontWeight.Bold
+                ),
+            )
+        },
+        navigationIcon = {
+            IconButton(
+                onClick = { viewModel.onEvent(PokemonDetailUIEvent.OnClickedBack) }
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_arrow_back),
+                    contentDescription = stringResource(R.string.icon_back)
+                )
+            }
+        },
+        actions = {
+            IconToggleButton(
+                checked = state.isFavorite,
+                onCheckedChange = {
+                    viewModel.onEvent(PokemonDetailUIEvent.OnClickedToggleFavorite)
+                }
+            ) {
+                Icon(
+                    painter = if (state.isFavorite)
+                        painterResource(R.drawable.ic_favorite)
+                    else
+                        painterResource(R.drawable.ic_favorite_border),
+                    tint = if (state.isFavorite)
+                        Color.Red
+                    else
+                        Color.LightGray,
+                    contentDescription = stringResource(R.string.icon_toggle_favorite),
+                )
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color.Transparent
+        ),
+    )
 }
