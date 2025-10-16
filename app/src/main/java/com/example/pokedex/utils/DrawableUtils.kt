986b5bf79a45d496d0ba3d35a3594e187e5357c2
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-16 12:42:52 -0600

    Config Pokemon Type icons part. 43.

--- app/src/main/java/com/example/pokedex/utils/DrawableUtils.kt
+++ app/src/main/java/com/example/pokedex/utils/DrawableUtils.kt
@@ -6,24 +6,24 @@ import com.example.pokedex.R
 object DrawableUtils {
     val String.icon : Int @DrawableRes get() =
         when (this) {
-            "bug" -> R.drawable.ic_type_bug
-            "dark" -> R.drawable.ic_type_dark
-            "dragon" -> R.drawable.ic_type_dragon
-            "electric" -> R.drawable.ic_type_electric
-            "fairy" -> R.drawable.ic_type_fairy
-            "fighting" -> R.drawable.ic_type_fighting
-            "fire" -> R.drawable.ic_type_fire
-            "flying" -> R.drawable.ic_type_flying
-            "ghost" -> R.drawable.ic_type_ghost
-            "grass" -> R.drawable.ic_type_grass
-            "ground" -> R.drawable.ic_type_ground
-            "ice" -> R.drawable.ic_type_ice
-            "normal" -> R.drawable.ic_type_normal
-            "poison" -> R.drawable.ic_type_poison
-            "psychic" -> R.drawable.ic_type_psychic
-            "rock" -> R.drawable.ic_type_rock
-            "steel" -> R.drawable.ic_type_steel
-            "water" -> R.drawable.ic_type_water
-            else -> R.drawable.ic_pokeball_splash
+            "Bug" -> R.drawable.ic_type_bug
+            "Dark" -> R.drawable.ic_type_dark
+            "Dragon" -> R.drawable.ic_type_dragon
+            "Electric" -> R.drawable.ic_type_electric
+            "Fairy" -> R.drawable.ic_type_fairy
+            "Fighting" -> R.drawable.ic_type_fighting
+            "Fire" -> R.drawable.ic_type_fire
+            "Flying" -> R.drawable.ic_type_flying
+            "Ghost" -> R.drawable.ic_type_ghost
+            "Grass" -> R.drawable.ic_type_grass
+            "Ground" -> R.drawable.ic_type_ground
+            "Ice" -> R.drawable.ic_type_ice
+            "Normal" -> R.drawable.ic_type_normal
+            "Poison" -> R.drawable.ic_type_poison
+            "Psychic" -> R.drawable.ic_type_psychic
+            "Rock" -> R.drawable.ic_type_rock
+            "Steel" -> R.drawable.ic_type_steel
+            "Water" -> R.drawable.ic_type_water
+            else -> R.drawable.ic_arrow_back
         }
 }
