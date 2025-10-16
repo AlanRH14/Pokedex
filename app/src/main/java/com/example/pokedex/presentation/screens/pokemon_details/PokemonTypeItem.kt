5b34470073d79795ff50a1b753211c37f694d897
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-16 12:26:26 -0600

    Config Pokemon Type icons part. 32.

--- app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonTypeItem.kt
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonTypeItem.kt
@@ -1,18 +1,23 @@
 package com.example.pokedex.presentation.screens.pokemon_details
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.pokedex.utils.DrawableUtils.icon
 
 @Composable
 fun PokemonTypes(ability: String) {
-    Text(
+    Row(
         modifier = Modifier
             .border(
                 width = 1.dp,
@@ -23,8 +28,15 @@ fun PokemonTypes(ability: String) {
                 horizontal = 16.dp,
                 vertical = 8.dp
             ),
-        text = ability,
-        color = Color.White,
-        style = MaterialTheme.typography.bodyLarge,
-    )
+    ) {
+        Icon(
+            painter = painterResource(id = ability.icon),
+            contentDescription = "Icon $ability",
+        )
+        Text(
+            text = ability,
+            color = Color.White,
+            style = MaterialTheme.typography.bodyLarge,
+        )
+    }
 }
