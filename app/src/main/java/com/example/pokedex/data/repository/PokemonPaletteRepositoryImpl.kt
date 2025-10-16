7228614cfb9043b8c68bd8cfa9574baa4bf6eae8
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-16 17:00:05 -0600

    Config Pokemon Item Palette generator config part. 13.

---
+++ app/src/main/java/com/example/pokedex/data/repository/PokemonPaletteRepositoryImpl.kt
@@ -0,0 +1,10 @@
+package com.example.pokedex.data.repository
+
+import com.example.pokedex.data.remote.ImageRemoteDataSource
+import com.example.pokedex.data.remote.PaletteDataSource
+
+class PokemonPaletteRepositoryImpl(
+    private val imageRemoteDataSource: ImageRemoteDataSource,
+    private val paletteDataSource: PaletteDataSource
+) {
+}
