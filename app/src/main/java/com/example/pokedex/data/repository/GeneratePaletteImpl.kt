0403c8b1261794e4813eecfea4ae80e74c53e684
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-16 16:55:56 -0600

    Config Pokemon Item Palette generator config part. 12.

--- app/src/main/java/com/example/pokedex/data/repository/GeneratePaletteImpl.kt
+++ app/src/main/java/com/example/pokedex/data/repository/GeneratePaletteImpl.kt
@@ -3,13 +3,10 @@ package com.example.pokedex.data.repository
 import android.graphics.Color
 import androidx.palette.graphics.Palette
 import coil3.Bitmap
-import com.example.pokedex.data.remote.ImageRemoteDataSource
 import com.example.pokedex.data.remote.PaletteDataSource
 import com.example.pokedex.domain.models.PokemonPaletteColors
 
-class GeneratePaletteImpl(
-    private val imageRemoteDataSource: ImageRemoteDataSource
-): PaletteDataSource {
+class GeneratePaletteImpl: PaletteDataSource {
 
     override suspend fun generatePalette(bitmap: Bitmap): PokemonPaletteColors {
         val palette = Palette.from(bitmap).generate()
