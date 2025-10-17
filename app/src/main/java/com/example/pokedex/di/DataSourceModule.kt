75d1d5487e7d4847b4a43a86c66657be36250051
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-17 12:28:21 -0600

    Config Pokemon Item Palette generator config part. 30.

--- app/src/main/java/com/example/pokedex/di/DataSourceModule.kt
+++ app/src/main/java/com/example/pokedex/di/DataSourceModule.kt
@@ -1,11 +1,13 @@
 package com.example.pokedex.di
 
 import com.example.pokedex.data.remote.ImageRemoteDataSource
+import com.example.pokedex.data.remote.PaletteDataSource
 import com.example.pokedex.data.repository.BitmapFromURLDataSourceImpl
+import com.example.pokedex.data.repository.PaletteIDataSourceImpl
 import org.koin.dsl.module
 
 
 val dataSourceModule = module {
     single<ImageRemoteDataSource> { BitmapFromURLDataSourceImpl() }
-
+    single<PaletteDataSource> { PaletteIDataSourceImpl() }
 }
