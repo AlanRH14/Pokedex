f8706efe239ec08569a23635878bbf414a31f5a7
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-20 13:45:49 -0600

    Padding pokemon item part. 13.

--- app/src/main/java/com/example/pokedex/presentation/screens/pokedex/components/PokemonItemShimmer.kt
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokedex/components/PokemonItemShimmer.kt
@@ -1,8 +1,81 @@
 package com.example.pokedex.presentation.screens.pokedex.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.example.pokedex.presentation.components.PokemonImage
+import com.example.pokedex.presentation.screens.pokedex.mvi.PokemonUIEvent
+import com.example.pokedex.utils.TYPE_COLOR
 
 @Composable
 fun PokemonItemShimmer() {
+    Card(
+        modifier = Modifier,
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White,
+            contentColor = MaterialTheme.colorScheme.onBackground
+        ),
+        shape = MaterialTheme.shapes.large,
+        elevation = CardDefaults.cardElevation(4.dp),
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .background(Color.Transparent)
+                .padding(4.dp)
+        ) {
 
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 4.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Box(
+                    modifier = Modifier
+                        .weight(1F),
+                    color = onDomainColor.copy(alpha = 0.8F),
+                )
+
+                Text(
+                    text = pokemon.id,
+                    textAlign = TextAlign.End,
+                    color = onDomainColor.copy(alpha = 0.5F),
+                    style = MaterialTheme.typography.titleMedium.copy(
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+            }
+
+            PokemonImage(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1.2f)
+                    .fillMaxHeight(),
+                image = pokemon.url,
+                contentScale = ContentScale.Fit,
+                contentDescription = "Image ${pokemon.name}",
+            )
+        }
+    }
 }
