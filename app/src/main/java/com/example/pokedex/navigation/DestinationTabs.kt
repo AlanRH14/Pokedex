6c3270ec9350b673a7626726c03fa0a986a4e32f
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-21 16:32:39 -0600

    Padding pokemon tabs info part. 18.

--- app/src/main/java/com/example/pokedex/navigation/DestinationTabs.kt
+++ app/src/main/java/com/example/pokedex/navigation/DestinationTabs.kt
@@ -2,6 +2,7 @@ package com.example.pokedex.navigation
 
 enum class DestinationTabs(val text: String) {
     INFORMATION(text = "Info."),
+    STATS(text = "Stats"),
     DEFENCE(text = "Defence"),
     EVOLUTION(text = "Evolution")
 }
