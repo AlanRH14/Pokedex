c58cd7d65a33ab28adf3a8db81a87d0e243ef73e
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-21 17:12:48 -0600

    Padding pokemon tabs info part. 31.

--- app/src/main/java/com/example/pokedex/navigation/TabsNavRoute.kt
+++ app/src/main/java/com/example/pokedex/navigation/TabsNavRoute.kt
@@ -1,8 +1,14 @@
 package com.example.pokedex.navigation
 
+import kotlinx.serialization.Serializable
+
 interface TabsNavRoute {
+    @Serializable
     data object About : TabsNavRoute
+    @Serializable
     data object Stats : TabsNavRoute
+    @Serializable
     data object Defence: TabsNavRoute
+    @Serializable
     data object Evolution: TabsNavRoute
 }
