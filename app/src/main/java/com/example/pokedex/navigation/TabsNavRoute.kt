bb79bcc62637e893ae6ce96b570b510413a85c50
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-21 17:05:38 -0600

    Padding pokemon tabs info part. 25.

--- app/src/main/java/com/example/pokedex/navigation/TabsNavRoute.kt
+++ app/src/main/java/com/example/pokedex/navigation/TabsNavRoute.kt
@@ -4,4 +4,5 @@ interface TabsNavRoute {
     data object About : TabsNavRoute
     data object Stats : TabsNavRoute
     data object Defence: TabsNavRoute
+    data object Evolution: TabsNavRoute
 }
