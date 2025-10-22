e39ffd4e760868eb058be5649447768895172fab
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-22 10:18:43 -0600

    Config Species Data part. 10.

--- app/src/main/java/com/example/pokedex/data/models/detail/Sprites.kt
+++ app/src/main/java/com/example/pokedex/data/models/detail/Sprites.kt
@@ -1,6 +1,6 @@
 package com.example.pokedex.data.models.detail
 
-import com.example.pokedex.data.models.OtherSprites
+import com.example.pokedex.data.models.detail.OtherSprites
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
