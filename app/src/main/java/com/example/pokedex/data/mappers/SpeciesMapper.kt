db75deecff853407b224434d0dddec86b4cfe573
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-24 13:53:25 -0600

    Config Species Mapper part. 8.

--- app/src/main/java/com/example/pokedex/data/mappers/SpeciesMapper.kt
+++ app/src/main/java/com/example/pokedex/data/mappers/SpeciesMapper.kt
@@ -3,7 +3,6 @@ package com.example.pokedex.data.mappers
 import com.example.pokedex.common.ApiMapper
 import com.example.pokedex.data.models.species.FlavorTextEntry
 import com.example.pokedex.data.models.species.Genera
-import com.example.pokedex.data.models.species.Language
 import com.example.pokedex.data.models.species.SpeciesResponse
 import com.example.pokedex.domain.models.Species
 
