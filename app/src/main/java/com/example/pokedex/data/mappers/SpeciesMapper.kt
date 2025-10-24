e148359095bb67922c4cb5027f4e161b26a575a5
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-24 13:53:12 -0600

    Config Species Mapper part. 8.

--- app/src/main/java/com/example/pokedex/data/mappers/SpeciesMapper.kt
+++ app/src/main/java/com/example/pokedex/data/mappers/SpeciesMapper.kt
@@ -1,14 +1,48 @@
 package com.example.pokedex.data.mappers
 
 import com.example.pokedex.common.ApiMapper
+import com.example.pokedex.data.models.species.FlavorTextEntry
+import com.example.pokedex.data.models.species.Genera
+import com.example.pokedex.data.models.species.Language
 import com.example.pokedex.data.models.species.SpeciesResponse
 import com.example.pokedex.domain.models.Species
 
-class SpeciesMapper: ApiMapper<SpeciesResponse, Species> {
+class SpeciesMapper : ApiMapper<SpeciesResponse, Species> {
 
     override fun mapperToDomain(dto: SpeciesResponse): Species {
         return Species(
-
+            id = dto.id ?: 0,
+            name = dto.name ?: "",
+            baseHappiness = dto.baseHappiness ?: 0,
+            captureRate = dto.captureRate ?: 0,
+            idEvolutionChain = "",
+            description = dto.flavorTextEntries?.getDescription() ?: "",
+            pokemonClass = dto.genera?.getPokemonClass() ?: "",
+            habitat = dto.habitat?.name ?: "",
+            isBaby = dto.isBaby ?: false,
+            isLegendary = dto.isLegendary ?: false,
+            isMythical = dto.isMythical ?: false
         )
+    }
+
+    private fun List<FlavorTextEntry>.getDescription(): String {
+        var description = ""
+        this.forEach {
+            if (it.language?.name == "en") {
+                description = it.flavorText ?: ""
+            }
+        }
+
+        return description
+    }
+
+    private fun List<Genera>.getPokemonClass(): String {
+        var genera = ""
+        this.forEach {
+            if (it.language?.name == "en") {
+                genera = it.genus ?: ""
+            }
+        }
+        return genera
     }
 }
