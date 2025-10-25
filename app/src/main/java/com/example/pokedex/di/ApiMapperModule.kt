4332bdf92a398883602cf37c0b16a8b047409711
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-10-25 00:33:12 -0600

    Config Species PokemonDetailImpl part. 12.

--- app/src/main/java/com/example/pokedex/di/ApiMapperModule.kt
+++ app/src/main/java/com/example/pokedex/di/ApiMapperModule.kt
@@ -3,14 +3,18 @@ package com.example.pokedex.di
 import com.example.pokedex.common.ApiMapper
 import com.example.pokedex.data.mappers.PokemonDetailMapperImpl
 import com.example.pokedex.data.mappers.PokemonMapperImpl
+import com.example.pokedex.data.mappers.SpeciesMapper
 import com.example.pokedex.data.models.detail.PokemonDetailDto
 import com.example.pokedex.data.models.pokemon.PokemonResponse
+import com.example.pokedex.data.models.species.SpeciesResponse
 import com.example.pokedex.domain.models.Pokemon
 import com.example.pokedex.domain.models.PokemonDetail
+import com.example.pokedex.domain.models.Species
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val apiMapperModule = module {
     single<ApiMapper<PokemonResponse, List<Pokemon>>>(named("PokemonMapperImpl")) { PokemonMapperImpl() }
     single<ApiMapper<PokemonDetailDto, PokemonDetail>>(named("PokemonDetailMapperImpl")) { PokemonDetailMapperImpl() }
+    single<ApiMapper<SpeciesResponse, Species>>(named("SpeciesMapperImpl")) { SpeciesMapper() }
 }
