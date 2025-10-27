fd0a8edfe598ab2e87e267cd58051cd6e5d41c31
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-27 12:19:32 -0600

    Config Defence Pokemon Types Models part. 32.

--- app/src/main/java/com/example/pokedex/data/models/type/GenerationIx.kt
+++ app/src/main/java/com/example/pokedex/data/models/type/GenerationIx.kt
@@ -1,11 +1,12 @@
 package com.example.pokedex.data.models.type
 
 
+import com.example.pokedex.data.models.type.common.NameIconBase
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class GenerationIx(
     @SerialName("scarlet-violet")
-    val scarletViolet: ScarletViolet?
+    val scarletViolet: NameIconBase?
 )
