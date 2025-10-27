cd3ca55996eca251d12067be2e7691777e823d12
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-27 12:10:00 -0600

    Config Defence Pokemon Types Models part. 28.

--- app/src/main/java/com/example/pokedex/data/models/type/GameIndices.kt
+++ app/src/main/java/com/example/pokedex/data/models/type/GameIndices.kt
@@ -1,6 +1,7 @@
 package com.example.pokedex.data.models.type
 
 
+import com.example.pokedex.data.models.type.common.NameUrlBase
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -9,5 +10,5 @@ data class GameIndices(
     @SerialName("game_index")
     val gameIndex: Int?,
     @SerialName("generation")
-    val generation: GenerationX?
+    val generation: NameUrlBase?
 )
