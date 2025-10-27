d74546f15c10c38ac77a40c9712634053d4b3838
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-10-26 22:13:36 -0600

    Config Species PokemonDetailImpl part. 25.

--- app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/widgets/PokemonAbout.kt
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/widgets/PokemonAbout.kt
@@ -1,11 +1,19 @@
 package com.example.pokedex.presentation.screens.pokemon_details.widgets
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.example.pokedex.domain.models.Species
 
 @Composable
@@ -17,7 +25,60 @@ fun PokemonAbout(
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Text(
-            text = species?.description ?: "Unknown"
+            text = species?.description ?: "Unknown",
+            style = MaterialTheme.typography.bodyLarge
         )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                modifier = Modifier.weight(0.2F),
+                text = "Habitat",
+                style = MaterialTheme.typography.bodyLarge.copy(
+                    fontWeight = FontWeight.Bold
+                )
+            )
+
+            Text(
+                modifier = Modifier.weight(0.8F),
+                text = species?.habitat ?: "Unknown",
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                modifier = Modifier.weight(0.2F),
+                text = "Happiness",
+                style = MaterialTheme.typography.bodyLarge.copy(
+                    fontWeight = FontWeight.Bold
+                )
+            )
+
+            Text(
+                modifier = Modifier.weight(0.8F),
+                text = "${species?.baseHappiness ?: "Unknown"}",
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                modifier = Modifier.weight(0.2F),
+                text = "Capture Rate",
+                style = MaterialTheme.typography.bodyLarge.copy(
+                    fontWeight = FontWeight.Bold
+                )
+            )
+
+            Text(
+                modifier = Modifier.weight(0.8F),
+                text = "${species?.captureRate ?: "Unknown"}",
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
     }
 }
