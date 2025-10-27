d38834b96529ff1a948ebf86ff57727f398ef7de
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-10-26 22:18:20 -0600

    Config Species PokemonDetailImpl part. 32.

--- app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/widgets/PokemonAbout.kt
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/widgets/PokemonAbout.kt
@@ -1,5 +1,6 @@
 package com.example.pokedex.presentation.screens.pokemon_details.widgets
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -30,7 +31,10 @@ fun PokemonAbout(
 
         Spacer(modifier = Modifier.height(8.dp))
 
-        Row(modifier = Modifier.fillMaxWidth()) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
             Text(
                 modifier = Modifier.weight(0.2F),
                 text = "Habitat:",
