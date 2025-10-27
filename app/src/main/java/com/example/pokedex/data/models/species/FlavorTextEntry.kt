669246d56487639ff2b24d17ca0f90439f18ac4f
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-27 12:50:35 -0600

    Config Defence Pokemon Types Models part. 57.

--- app/src/main/java/com/example/pokedex/data/models/species/FlavorTextEntry.kt
+++ app/src/main/java/com/example/pokedex/data/models/species/FlavorTextEntry.kt
@@ -1,6 +1,7 @@
 package com.example.pokedex.data.models.species
 
 
+import com.example.pokedex.data.models.type.common.NameUrlBase
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -9,7 +10,7 @@ data class FlavorTextEntry(
     @SerialName("flavor_text")
     val flavorText: String?,
     @SerialName("language")
-    val language: Language?,
+    val language: NameUrlBase?,
     @SerialName("version")
     val version: Version?
 )
