d82fb421ece793c87e3a75df626534cb55b117c6
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-27 11:57:25 -0600

    Config Defence Pokemon Types Models part. 23.

--- app/src/main/java/com/example/pokedex/data/models/type/DamageRelations.kt
+++ app/src/main/java/com/example/pokedex/data/models/type/DamageRelations.kt
@@ -11,11 +11,11 @@ data class DamageRelations(
     @SerialName("double_damage_to")
     val doubleDamageTo: List<Damage?>?,
     @SerialName("half_damage_from")
-    val halfDamageFrom: List<HalfDamageFrom?>?,
+    val halfDamageFrom: List<Damage?>?,
     @SerialName("half_damage_to")
-    val halfDamageTo: List<HalfDamageTo?>?,
+    val halfDamageTo: List<Damage?>?,
     @SerialName("no_damage_from")
-    val noDamageFrom: List<NoDamageFrom?>?,
+    val noDamageFrom: List<Damage?>?,
     @SerialName("no_damage_to")
     val noDamageTo: List<NoDamageTo?>?
 )
