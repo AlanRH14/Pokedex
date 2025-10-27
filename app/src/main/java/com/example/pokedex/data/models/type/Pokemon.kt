3c2a00aee81eb1f784baa7818a30ab55ff9e93eb
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-27 12:24:25 -0600

    Config Defence Pokemon Types Models part. 36.

--- app/src/main/java/com/example/pokedex/data/models/type/Pokemon.kt
+++ app/src/main/java/com/example/pokedex/data/models/type/Pokemon.kt
@@ -1,13 +1,13 @@
 package com.example.pokedex.data.models.type
 
-
+import com.example.pokedex.data.models.type.common.NameUrlBase
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Pokemon(
     @SerialName("pokemon")
-    val pokemon: PokemonX?,
+    val pokemon: NameUrlBase?,
     @SerialName("slot")
     val slot: Int?
 )
