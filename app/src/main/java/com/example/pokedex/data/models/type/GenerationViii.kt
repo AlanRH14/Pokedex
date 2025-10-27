bde8671c1de305f5ab2cb1cc0432ffb0ae48282f
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-27 12:37:04 -0600

    Config Defence Pokemon Types Models part. 45.

--- app/src/main/java/com/example/pokedex/data/models/type/GenerationViii.kt
+++ app/src/main/java/com/example/pokedex/data/models/type/GenerationViii.kt
@@ -8,9 +8,9 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class GenerationViii(
     @SerialName("brilliant-diamond-and-shining-pearl")
-    val brilliantDiamondAndShiningPearl: BrilliantDiamondAndShiningPearl?,
+    val brilliantDiamondAndShiningPearl: NameIconBase?,
     @SerialName("legends-arceus")
-    val legendsArceus: LegendsArceus?,
+    val legendsArceus: NameIconBase?,
     @SerialName("sword-shield")
     val swordShield: NameIconBase?
 )
