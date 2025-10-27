e43e40997870566ab9a79842bce07d9f3e3af702
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-27 12:38:09 -0600

    Config Defence Pokemon Types Models part. 46.

--- app/src/main/java/com/example/pokedex/data/models/type/DamageRelations.kt
+++ app/src/main/java/com/example/pokedex/data/models/type/DamageRelations.kt
@@ -1,21 +1,21 @@
 package com.example.pokedex.data.models.type
 
-
+import com.example.pokedex.data.models.type.common.NameUrlBase
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class DamageRelations(
     @SerialName("double_damage_from")
-    val damage: List<Damage?>?,
+    val damage: List<NameUrlBase?>?,
     @SerialName("double_damage_to")
-    val doubleDamageTo: List<Damage?>?,
+    val doubleDamageTo: List<NameUrlBase?>?,
     @SerialName("half_damage_from")
-    val halfDamageFrom: List<Damage?>?,
+    val halfDamageFrom: List<NameUrlBase?>?,
     @SerialName("half_damage_to")
-    val halfDamageTo: List<Damage?>?,
+    val halfDamageTo: List<NameUrlBase?>?,
     @SerialName("no_damage_from")
-    val noDamageFrom: List<Damage?>?,
+    val noDamageFrom: List<NameUrlBase?>?,
     @SerialName("no_damage_to")
-    val noDamageTo: List<Damage?>?
+    val noDamageTo: List<NameUrlBase?>?
 )
