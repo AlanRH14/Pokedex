19633460fa2720535260cac48fc992acf049f32c
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-28 17:33:54 -0600

    Config Defence Pokemon Types Mapper part. 12.

--- app/src/main/java/com/example/pokedex/di/ApiMapperModule.kt
+++ app/src/main/java/com/example/pokedex/di/ApiMapperModule.kt
@@ -4,11 +4,14 @@ import com.example.pokedex.common.ApiMapper
 import com.example.pokedex.data.mappers.PokemonDetailMapperImpl
 import com.example.pokedex.data.mappers.PokemonMapperImpl
 import com.example.pokedex.data.mappers.SpeciesMapperImpl
+import com.example.pokedex.data.mappers.TypeMapperImpl
 import com.example.pokedex.data.models.detail.PokemonDetailDto
 import com.example.pokedex.data.models.pokemon.PokemonResponse
 import com.example.pokedex.data.models.species.SpeciesResponse
+import com.example.pokedex.data.models.type.TypeDto
 import com.example.pokedex.domain.models.Pokemon
 import com.example.pokedex.domain.models.PokemonDetail
+import com.example.pokedex.domain.models.PokemonType
 import com.example.pokedex.domain.models.Species
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
@@ -17,4 +20,5 @@ val apiMapperModule = module {
     single<ApiMapper<PokemonResponse, List<Pokemon>>>(named("PokemonMapperImpl")) { PokemonMapperImpl() }
     single<ApiMapper<PokemonDetailDto, PokemonDetail>>(named("PokemonDetailMapperImpl")) { PokemonDetailMapperImpl() }
     single<ApiMapper<SpeciesResponse, Species>>(named("SpeciesMapperImpl")) { SpeciesMapperImpl() }
+    single<ApiMapper<TypeDto, PokemonType>>(named("PokemonTypeMapperImpl")) { TypeMapperImpl() }
 }
