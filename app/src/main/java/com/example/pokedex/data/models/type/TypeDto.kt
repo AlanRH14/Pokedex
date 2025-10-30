f5a93d9c0030cc97286f665ef53d72868dbf5680
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-10-30 09:58:22 -0600

    Config Defence Pokemon Types ViewModel config part. 7.

--- app/src/main/java/com/example/pokedex/data/models/type/TypeDto.kt
+++ app/src/main/java/com/example/pokedex/data/models/type/TypeDto.kt
@@ -7,25 +7,25 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class TypeDto(
     @SerialName("damage_relations")
-    val damageRelations: DamageRelations?,
+    val damageRelations: DamageRelations? = null,
     @SerialName("game_indices")
-    val gameIndices: List<GameIndices>?,
+    val gameIndices: List<GameIndices>? = null,
     @SerialName("generation")
-    val generation: NameUrlBase?,
+    val generation: NameUrlBase? = null,
     @SerialName("id")
-    val id: Int?,
+    val id: Int? = null,
     @SerialName("move_damage_class")
-    val moveDamageClass: NameUrlBase?,
+    val moveDamageClass: NameUrlBase? = null,
     @SerialName("moves")
-    val moves: List<NameUrlBase>?,
+    val moves: List<NameUrlBase>? = null,
     @SerialName("name")
-    val name: String?,
+    val name: String? = null,
     @SerialName("names")
-    val names: List<Name>?,
+    val names: List<Name>? = null,
     @SerialName("past_damage_relations")
-    val pastDamageRelations: List<PastDamageRelation>?,
+    val pastDamageRelations: List<PastDamageRelation>? = null,
     @SerialName("pokemon")
-    val pokemon: List<Pokemon>?,
+    val pokemon: List<Pokemon>? = null,
     @SerialName("sprites")
-    val sprites: Sprites?
+    val sprites: Sprites? = null
 )
