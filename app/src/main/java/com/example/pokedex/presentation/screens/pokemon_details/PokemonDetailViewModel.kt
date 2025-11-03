e7ba9b2d04b003da29a82042b5f9a7abed8b871e
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-11-03 13:04:15 -0600

    Config Defence Pokemon Types ViewModel config part. 34.

--- app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonDetailViewModel.kt
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/PokemonDetailViewModel.kt
@@ -37,7 +37,7 @@ class PokemonDetailViewModel(
             is PokemonDetailUIEvent.OnClickedToggleFavorite -> changeToggleFavoriteState()
             is PokemonDetailUIEvent.OnClickedBack -> navigateToBack()
             is PokemonDetailUIEvent.OnClickTabNavigation -> navigateToTabs(route = event.route)
-            is PokemonDetailUIEvent.OnGetPokemonType -> getPokemonType(pokemonID = event.pokemonID)
+            is PokemonDetailUIEvent.OnGetPokemonType -> getPokemonType(types = event.type)
         }
     }
 
