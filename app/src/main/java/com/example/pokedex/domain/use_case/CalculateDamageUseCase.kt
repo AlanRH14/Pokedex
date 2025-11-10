9b0e3571955c3c2413e810a784ac6d989cfd3e59
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-11-10 13:54:43 -0600

    Config Defence Pokemon Types ViewModel config part. 45.

--- app/src/main/java/com/example/pokedex/domain/use_case/CalculateDamageUseCase.kt
+++ app/src/main/java/com/example/pokedex/domain/use_case/CalculateDamageUseCase.kt
@@ -3,7 +3,6 @@ package com.example.pokedex.domain.use_case
 import com.example.pokedex.domain.models.PokemonType
 
 class CalculateDamageUseCase {
-
     fun calculateDefenseMultiplier(types: List<PokemonType>): Map<String, Double> {
         if (types.isEmpty()) return emptyMap()
         val allAttackTypes = mutableSetOf<String>()
