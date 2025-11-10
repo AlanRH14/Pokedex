61dad77eb53e79edbb9d836cc90ba23b367ab438
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-11-10 17:37:23 -0600

    Config Defence Pokemon Types ViewModel config part. 52.

--- app/src/main/java/com/example/pokedex/data/mappers/TypeMapperImpl.kt
+++ app/src/main/java/com/example/pokedex/data/mappers/TypeMapperImpl.kt
@@ -3,6 +3,7 @@ package com.example.pokedex.data.mappers
 import com.example.pokedex.common.ApiMapper
 import com.example.pokedex.data.models.type.TypeDto
 import com.example.pokedex.domain.models.PokemonType
+import com.example.pokedex.utils.StringUtils.capitalized
 
 class TypeMapperImpl : ApiMapper<TypeDto, PokemonType> {
 
@@ -10,17 +11,17 @@ class TypeMapperImpl : ApiMapper<TypeDto, PokemonType> {
         return PokemonType(
             id = dto.id ?: 0,
             name = dto.name ?: "",
-            doubleDamageFrom = dto.damageRelations?.doubleDamageFrom?.map { it.name ?: "" }
+            doubleDamageFrom = dto.damageRelations?.doubleDamageFrom?.map { (it.name ?: "").capitalized() }
                 ?: emptyList(),
-            doubleDamageTo = dto.damageRelations?.doubleDamageTo?.map { it.name ?: "" }
+            doubleDamageTo = dto.damageRelations?.doubleDamageTo?.map { (it.name ?: "").capitalized() }
                 ?: emptyList(),
-            halfDamageFrom = dto.damageRelations?.halfDamageFrom?.map { it.name ?: "" }
+            halfDamageFrom = dto.damageRelations?.halfDamageFrom?.map { (it.name ?: "").capitalized() }
                 ?: emptyList(),
-            halfDamageTo = dto.damageRelations?.doubleDamageTo?.map { it.name ?: "" }
+            halfDamageTo = dto.damageRelations?.doubleDamageTo?.map { (it.name ?: "").capitalized() }
                 ?: emptyList(),
-            noDamageFrom = dto.damageRelations?.noDamageFrom?.map { it.name ?: "" }
+            noDamageFrom = dto.damageRelations?.noDamageFrom?.map { (it.name ?: "").capitalized() }
                 ?: emptyList(),
-            noDamageTo = dto.damageRelations?.noDamageTo?.map { it.name ?: "" }
+            noDamageTo = dto.damageRelations?.noDamageTo?.map { (it.name ?: "").capitalized() }
                 ?: emptyList(),
         )
     }
--- app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/widgets/PokemonDamage.kt
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/widgets/PokemonDamage.kt
@@ -3,7 +3,6 @@ package com.example.pokedex.presentation.screens.pokemon_details.widgets
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
