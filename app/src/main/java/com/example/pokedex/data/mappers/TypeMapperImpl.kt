92c7c7f13ae2e866f7bcd54122ae5f1a19ed47c6
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-11-11 11:51:27 -0600

    Config Defence Pokemon Types ViewModel config part. 54.

--- app/src/main/java/com/example/pokedex/data/mappers/TypeMapperImpl.kt
+++ app/src/main/java/com/example/pokedex/data/mappers/TypeMapperImpl.kt
@@ -3,6 +3,7 @@ package com.example.pokedex.data.mappers
 import com.example.pokedex.common.ApiMapper
 import com.example.pokedex.data.models.type.TypeDto
 import com.example.pokedex.domain.models.PokemonType
+import com.example.pokedex.domain.models.Type
 import com.example.pokedex.utils.StringUtils.capitalized
 
 class TypeMapperImpl : ApiMapper<TypeDto, PokemonType> {
@@ -11,18 +12,24 @@ class TypeMapperImpl : ApiMapper<TypeDto, PokemonType> {
         return PokemonType(
             id = dto.id ?: 0,
             name = dto.name ?: "",
-            doubleDamageFrom = dto.damageRelations?.doubleDamageFrom?.map { (it.name ?: "").capitalized() }
-                ?: emptyList(),
-            doubleDamageTo = dto.damageRelations?.doubleDamageTo?.map { (it.name ?: "").capitalized() }
-                ?: emptyList(),
-            halfDamageFrom = dto.damageRelations?.halfDamageFrom?.map { (it.name ?: "").capitalized() }
-                ?: emptyList(),
-            halfDamageTo = dto.damageRelations?.doubleDamageTo?.map { (it.name ?: "").capitalized() }
-                ?: emptyList(),
-            noDamageFrom = dto.damageRelations?.noDamageFrom?.map { (it.name ?: "").capitalized() }
-                ?: emptyList(),
-            noDamageTo = dto.damageRelations?.noDamageTo?.map { (it.name ?: "").capitalized() }
-                ?: emptyList(),
+            doubleDamageFrom = dto.damageRelations?.doubleDamageFrom?.map {
+                Type(type = (it.name ?: "").capitalized())
+            } ?: emptyList(),
+            doubleDamageTo = dto.damageRelations?.doubleDamageTo?.map {
+                Type(type = (it.name ?: "").capitalized())
+            } ?: emptyList(),
+            halfDamageFrom = dto.damageRelations?.halfDamageFrom?.map {
+                Type(type = (it.name ?: "").capitalized())
+            } ?: emptyList(),
+            halfDamageTo = dto.damageRelations?.doubleDamageTo?.map {
+                Type(type = (it.name ?: "").capitalized())
+            } ?: emptyList(),
+            noDamageFrom = dto.damageRelations?.noDamageFrom?.map {
+                Type(type = (it.name ?: "").capitalized())
+            } ?: emptyList(),
+            noDamageTo = dto.damageRelations?.noDamageTo?.map {
+                Type(type = (it.name ?: "").capitalized())
+            } ?: emptyList(),
         )
     }
 }
--- app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/widgets/PokemonDamage.kt
+++ app/src/main/java/com/example/pokedex/presentation/screens/pokemon_details/widgets/PokemonDamage.kt
@@ -1,21 +1,18 @@
 package com.example.pokedex.presentation.screens.pokemon_details.widgets
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.pokedex.presentation.screens.pokemon_details.components.PokemonTypeItem
+import com.example.pokedex.domain.models.Type
 
 @Composable
 fun PokemonDamage(
-    damages: Map<Double, List<String>>? = null,
+    damages: Map<Double, List<Type>>? = null,
     color: Int
 ) {
     if (damages == null) return
@@ -28,14 +25,11 @@ fun PokemonDamage(
     ) {
         damages.entries.forEach { (damage, types) ->
             Text(text = "x$damage")
-            LazyRow(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-                items(types) { type ->
-                    PokemonTypeItem(
-                        types = type,
-                        color = color
-                    )
-                }
-            }
+
+            PokemonTypes(
+                types = types,
+                color = color
+            )
         }
     }
 }
