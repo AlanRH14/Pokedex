856535db00d816a11eb0074eed84bb78cd63d717
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-11-18 11:17:51 -0600

    Config libs paging part. 4.

--- app/src/main/java/com/example/pokedex/data/repository/PokemonMediator.kt
+++ app/src/main/java/com/example/pokedex/data/repository/PokemonMediator.kt
@@ -1,9 +1,42 @@
 package com.example.pokedex.data.repository
 
 import androidx.paging.PagingSource
+import androidx.paging.PagingState
 import com.example.pokedex.data.remote.PokedexService
+import com.example.pokedex.domain.models.Pokemon
 
 class PokemonMediator(
     private val pokedexService: PokedexService,
-): PagingSource<Int, Pokemo> {
+) : PagingSource<Int, Pokemon>() {
+
+    override fun getRefreshKey(state: PagingState<Int, Pokemon>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition = anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition = anchorPosition)?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Pokemon> {
+        try {
+            when (params) {
+                is LoadParams.Refresh -> {}
+
+                is LoadParams.Prepend -> {}
+
+                is LoadParams.Append -> {}
+            }
+
+            val nextPageNumber = params.key ?: 1
+            val response = pokedexService.fetchPokemonList(offset = nextPageNumber)
+
+            LoadResult.Page(
+                data = listOf(),
+                prevKey = response.previous,
+                nextKey = response.next
+            )
+
+        } catch (e: Exception) {
+            return LoadResult.Error(throwable = e)
+        }
+    }
 }
