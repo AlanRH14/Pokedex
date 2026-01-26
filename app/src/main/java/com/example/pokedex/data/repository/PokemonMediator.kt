b5b707bc800d65fc877327cd7bce57be389fb2c3
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2026-01-26 10:56:13 -0600

    Update versions libs part. 1.

--- app/src/main/java/com/example/pokedex/data/repository/PokemonMediator.kt
+++ app/src/main/java/com/example/pokedex/data/repository/PokemonMediator.kt
@@ -2,21 +2,22 @@ package com.example.pokedex.data.repository
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
+import com.example.pokedex.data.models.pokemon.PokemonDto
 import com.example.pokedex.data.remote.PokedexService
 import com.example.pokedex.domain.models.Pokemon
 
-class PokemonMediator(
+/*class PokemonMediator(
     private val pokedexService: PokedexService,
-) : PagingSource<Int, Pokemon>() {
+) : PagingSource<Int, PokemonDto>() {
 
-    override fun getRefreshKey(state: PagingState<Int, Pokemon>): Int? {
+    override fun getRefreshKey(state: PagingState<Int, PokemonDto>): Int? {
         return state.anchorPosition?.let { anchorPosition ->
             state.closestPageToPosition(anchorPosition = anchorPosition)?.prevKey?.plus(1)
                 ?: state.closestPageToPosition(anchorPosition = anchorPosition)?.nextKey?.minus(1)
         }
     }
 
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Pokemon> {
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, PokemonDto> {
         try {
             when (params) {
                 is LoadParams.Refresh -> {}
@@ -29,14 +30,14 @@ class PokemonMediator(
             val nextPageNumber = params.key ?: 1
             val response = pokedexService.fetchPokemonList(offset = nextPageNumber)
 
-            LoadResult.Page(
-                data = listOf(),
-                prevKey = response.previous,
-                nextKey = response.next
+            return LoadResult.Page(
+                data = response?.results ?: emptyList(),
+                prevKey = response.previous ?: "",
+                nextKey = response.next ?: ""
             )
 
         } catch (e: Exception) {
             return LoadResult.Error(throwable = e)
         }
     }
-}
+}*/
