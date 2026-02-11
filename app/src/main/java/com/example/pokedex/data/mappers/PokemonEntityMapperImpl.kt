5a47787b2c4f60091cf01ea8b03b86d92e66398d
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2026-02-10 19:49:02 -0600

    Change PokemonMapper Implementation in PokemonMediator part. 3.

--- app/src/main/java/com/example/pokedex/data/mappers/PokemonEntityMapperImpl.kt
+++ app/src/main/java/com/example/pokedex/data/mappers/PokemonEntityMapperImpl.kt
@@ -3,10 +3,8 @@ package com.example.pokedex.data.mappers
 import com.example.pokedex.common.ApiMapper
 import com.example.pokedex.data.local.entity.PokemonEntity
 import com.example.pokedex.data.models.pokemon.PokemonResponse
-import com.example.pokedex.domain.models.Pokemon
 import com.example.pokedex.utils.Constants
 import com.example.pokedex.utils.StringUtils.capitalized
-import com.example.pokedex.utils.StringUtils.formatPokemonID
 import com.example.pokedex.utils.StringUtils.getIDFromURL
 
 class PokemonEntityMapperImpl : ApiMapper<PokemonResponse, List<PokemonEntity>> {
--- app/src/main/java/com/example/pokedex/data/mappers/PokemonMapperImpl.kt
+++ app/src/main/java/com/example/pokedex/data/mappers/PokemonMapperImpl.kt
@@ -2,14 +2,11 @@ package com.example.pokedex.data.mappers
 
 import com.example.pokedex.common.ApiMapper
 import com.example.pokedex.data.local.entity.PokemonEntity
-import com.example.pokedex.data.models.pokemon.PokemonResponse
 import com.example.pokedex.domain.models.Pokemon
-import com.example.pokedex.utils.Constants
-import com.example.pokedex.utils.StringUtils.capitalized
 import com.example.pokedex.utils.StringUtils.formatPokemonID
-import com.example.pokedex.utils.StringUtils.getIDFromURL
 
 class PokemonMapperImpl : ApiMapper<List<PokemonEntity>, List<Pokemon>> {
+
     override fun mapperToDomain(dto: List<PokemonEntity>): List<Pokemon> {
         return dto.map { pokemonEntity ->
             Pokemon(
