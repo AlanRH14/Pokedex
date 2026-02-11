087930c319c87f714ff3af439fcaef6c72bd5122
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2026-02-10 19:44:52 -0600

    Change PokemonMapper Implementation in PokemonMediator part. 2.

--- app/src/main/java/com/example/pokedex/data/mappers/PokemonEntityMapperImpl.kt
+++ app/src/main/java/com/example/pokedex/data/mappers/PokemonEntityMapperImpl.kt
@@ -2,19 +2,23 @@ package com.example.pokedex.data.mappers
 
 import com.example.pokedex.common.ApiMapper
 import com.example.pokedex.data.local.entity.PokemonEntity
+import com.example.pokedex.data.models.pokemon.PokemonResponse
 import com.example.pokedex.domain.models.Pokemon
+import com.example.pokedex.utils.Constants
+import com.example.pokedex.utils.StringUtils.capitalized
 import com.example.pokedex.utils.StringUtils.formatPokemonID
+import com.example.pokedex.utils.StringUtils.getIDFromURL
 
-class PokemonEntityMapperImpl : ApiMapper<List<PokemonEntity>, List<Pokemon>> {
+class PokemonEntityMapperImpl : ApiMapper<PokemonResponse, List<PokemonEntity>> {
 
-    override fun mapperToDomain(dto: List<PokemonEntity>): List<Pokemon> {
-        return dto.map { pokemonEntity ->
-            Pokemon(
-                id = pokemonEntity.id.formatPokemonID(),
-                name = pokemonEntity.name.,
-                url = pokemonEntity.url,
-                colorPalette = null,
+    override fun mapperToDomain(dto: PokemonResponse): List<PokemonEntity> {
+        return dto.results?.map { pokemonRes ->
+            val pokemonID = pokemonRes.url.getIDFromURL()
+            PokemonEntity(
+                id = pokemonID,
+                name = pokemonRes.name.capitalized(),
+                url = "${Constants.BAR_URL_IMAGE}${pokemonID}.png"
             )
-        }
+        } ?: emptyList()
     }
 }
--- app/src/main/java/com/example/pokedex/data/mappers/PokemonMapperImpl.kt
+++ app/src/main/java/com/example/pokedex/data/mappers/PokemonMapperImpl.kt
@@ -3,19 +3,21 @@ package com.example.pokedex.data.mappers
 import com.example.pokedex.common.ApiMapper
 import com.example.pokedex.data.local.entity.PokemonEntity
 import com.example.pokedex.data.models.pokemon.PokemonResponse
+import com.example.pokedex.domain.models.Pokemon
 import com.example.pokedex.utils.Constants
 import com.example.pokedex.utils.StringUtils.capitalized
+import com.example.pokedex.utils.StringUtils.formatPokemonID
 import com.example.pokedex.utils.StringUtils.getIDFromURL
 
-class PokemonMapperImpl : ApiMapper<PokemonResponse, List<PokemonEntity>> {
-    override fun mapperToDomain(dto: PokemonResponse): List<PokemonEntity> {
-        return dto.results?.map { pokemon ->
-            val pokemonID = pokemon.url.getIDFromURL()
-            PokemonEntity(
-                id = pokemonID,
-                name = pokemon.name.capitalized(),
-                url = "${Constants.BAR_URL_IMAGE}${pokemonID}.png"
+class PokemonMapperImpl : ApiMapper<List<PokemonEntity>, List<Pokemon>> {
+    override fun mapperToDomain(dto: List<PokemonEntity>): List<Pokemon> {
+        return dto.map { pokemonEntity ->
+            Pokemon(
+                id = pokemonEntity.id.formatPokemonID(),
+                name = pokemonEntity.name ?: "",
+                url = pokemonEntity.url ?: "",
+                colorPalette = null,
             )
-        } ?: emptyList()
+        }
     }
 }
